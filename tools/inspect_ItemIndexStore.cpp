55f4fb684b90e5172a9fd8595540a22075c6670e
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-05-28 15:52:11 +0200

    [tools] add option to huffman encode an index file

--- tools/inspect_ItemIndexStore.cpp
+++ tools/inspect_ItemIndexStore.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
+#include <boost-1_49/boost/concept_check.hpp>
 #include <sserialize/Static/ItemIndexStore.h>
 #include <sserialize/utility/statfuncs.h>
 #include <sserialize/utility/UByteArrayAdapter.h>
 #include <sserialize/templated/HuffmanTree.h>
+#include <sserialize/containers/MultiBitBackInserter.h>
+#include <sserialize/containers/SortedOffsetIndexPrivate.h>
 
 using namespace std;
 using namespace sserialize;
@@ -28,6 +31,87 @@ void incAlphabet(std::unordered_map<uint32_t, uint32_t> & a, uint32_t v) {
 		a[v] += 1;
 }
 
+void createAlphabet(sserialize::Static::ItemIndexStore & store, std::unordered_map<uint32_t, uint32_t> & alphabet, uint32_t & charSize) {
+	UByteArrayAdapter data = store.getData();
+	UByteArrayAdapter::OffsetType size = data.size();
+	if (store.indexType() == ItemIndex::T_WAH) {
+		charSize = 4;
+		for(UByteArrayAdapter::OffsetType i = 0; i < size; i += charSize) {
+			uint32_t character = data.getUint32(i);
+			incAlphabet(alphabet, character);
+		}
+	}
+	else if (store.indexType() == ItemIndex::T_RLE_DE) {
+		for(UByteArrayAdapter::OffsetType i = 0; i < size; ) {
+			uint32_t curIndexSize = data.getUint32(i);
+			i += 4;
+			incAlphabet(alphabet, curIndexSize);
+			uint32_t curIndexCount = data.getUint32(i);
+			i += 4;
+			incAlphabet(alphabet, curIndexCount);
+			UByteArrayAdapter indexData(data, i, curIndexSize);
+			while(indexData.tellGetPtr() < indexData.size()) {
+				uint32_t c = indexData.getVlPackedUint32();
+				incAlphabet(alphabet, c);
+			}
+			i += curIndexSize;
+		}
+	}
+}
+
+UByteArrayAdapter::OffsetType recompressIndexData(uint8_t alphabetBitLength, sserialize::Static::ItemIndexStore & store, UByteArrayAdapter & dest) {
+	if (store.indexType() != ItemIndex::T_WAH) {
+		std::cerr << "Unsupported index format" << std::endl;
+		return 0;
+	}
+	UByteArrayAdapter data = store.getData();
+	uint64_t size = data.size();
+	std::unordered_map<uint32_t, uint32_t> alphabet;
+	uint32_t charSize = 4;
+	createAlphabet(store, alphabet, charSize);
+	HuffmanTree<uint32_t> ht(alphabetBitLength);
+	ht.create(alphabet.begin(), alphabet.end(), size/charSize);
+	std::unordered_map<uint32_t, HuffmanCodePoint> htMap(ht.codePointMap());
+	
+	
+	//now recompress
+	UByteArrayAdapter::OffsetType beginOffset = dest.tellPutPtr();
+	dest.putUint8(0);
+	dest.putUint8(0);
+	dest.putOffset(0);
+	std::vector<UByteArrayAdapter::OffsetType> newOffsets;
+	newOffsets.reserve(store.size());
+	
+	MultiBitBackInserter backInserter(dest);
+	data.resetGetPtr();
+	ProgressInfo pinfo;
+	pinfo.begin(data.size(), "Encoding words");
+	while(data.getPtrHasNext()) {
+		newOffsets.push_back(backInserter.data().size()-7);
+		uint32_t indexSize = data.getUint32();
+		uint32_t indexCount = data.getUint32();
+		const HuffmanCodePoint & sizeCp = htMap.at(indexSize);
+		const HuffmanCodePoint & countCp = htMap.at(indexCount);
+		backInserter.push_back(sizeCp.code(), sizeCp.codeLength());
+		backInserter.push_back(countCp.code(), countCp.codeLength());
+		indexSize = indexSize / 4;
+		for(uint32_t i = 0; i < indexSize; ++i) {
+			uint32_t src = data.getUint32();
+			const HuffmanCodePoint & srcCp =  htMap.at(src);
+			backInserter.push_back(srcCp.code(), srcCp.codeLength());
+		}
+		backInserter.flush();
+		pinfo(data.tellGetPtr());
+	}
+	backInserter.flush();
+	dest = backInserter.data();
+	pinfo.end("Encoded words");
+	std::cout << "Creating offset index" << std::endl;
+	sserialize::Static::SortedOffsetIndexPrivate::create(newOffsets, dest);
+	std::cout << "Offset index created. Total size: " << dest.tellPutPtr()-beginOffset;
+	return dest.tellPutPtr()+7-beginOffset;
+}
+
 int main(int argc, char ** argv) {
 	std::string inFileName;
 	std::string outFileName;
@@ -35,6 +119,7 @@ int main(int argc, char ** argv) {
 	bool dumpIndexStoreIndex = false;
 	bool dumpDataHisto = false;
 	uint8_t alphabetBitLength = 1;
+	bool recompress = false;
 	
 	for(int i = 1; i < argc; i++) {
 		std::string curArg(argv[i]);
@@ -50,6 +135,8 @@ int main(int argc, char ** argv) {
 			alphabetBitLength = ::atoi(argv[i+1]);
 			++i;
 		}
+		if (curArg == "-rc")
+			recompress = true;
 		if (curArg == "-o" && i+1 < argc) {
 			outFileName = std::string(argv[i+1]);
 			i++;
@@ -107,33 +194,11 @@ int main(int argc, char ** argv) {
 		}
 	}
 	if (dumpDataHisto) {
-		UByteArrayAdapter data = store.getData();
-		UByteArrayAdapter::OffsetType size = data.size();
-		std::unordered_map<uint32_t, uint32_t> alphabet;
 		uint32_t charSize = 4;
-		if (store.indexType() == ItemIndex::T_WAH) {
-			charSize = 4;
-			for(UByteArrayAdapter::OffsetType i = 0; i < size; i += charSize) {
-				uint32_t character = data.getUint32(i);
-				incAlphabet(alphabet, character);
-			}
-		}
-		else if (store.indexType() == ItemIndex::T_RLE_DE) {
-			for(UByteArrayAdapter::OffsetType i = 0; i < size; ) {
-				uint32_t curIndexSize = data.getUint32(i);
-				i += 4;
-				incAlphabet(alphabet, curIndexSize);
-				uint32_t curIndexCount = data.getUint32(i);
-				i += 4;
-				incAlphabet(alphabet, curIndexCount);
-				UByteArrayAdapter indexData(data, i, curIndexSize);
-				while(indexData.tellGetPtr() < indexData.size()) {
-					uint32_t c = indexData.getVlPackedUint32();
-					incAlphabet(alphabet, c);
-				}
-				i += curIndexSize;
-			}
-		}
+		uint64_t size = store.getData().size();
+		std::unordered_map<uint32_t, uint32_t> alphabet;
+		createAlphabet(store, alphabet, charSize);
+	
 		std::cout << "#Data lenth: " << size << std::endl;
 		std::cout << "#Alphabet size: " << alphabet.size() << std::endl;
 		std::cout << "#String length: " << size/charSize << std::endl;
@@ -157,5 +222,19 @@ int main(int argc, char ** argv) {
 			bitUsage += htMap.at(it->first).codeLength()*it->second;
 		}
 		std::cout << bitUsage/8 << "(" << (double)(bitUsage/8) / size * 100 << "%)" << std::endl;
+	}
+	
+	if (recompress) {
+		std::string outFile;
+		if (outFileName.empty())
+			outFile = inFileName + ".htcmp";
+		else
+			outFile = outFileName;
+		UByteArrayAdapter outData(UByteArrayAdapter::createFile(adap.size(), outFile));
+		UByteArrayAdapter::OffsetType size = recompressIndexData(1, store, outData);
+		if (size > 0)
+			outData.shrinkStorage(adap.size()-size);
+		else
+			outData.setDeleteOnClose(true);
 	}
 }
