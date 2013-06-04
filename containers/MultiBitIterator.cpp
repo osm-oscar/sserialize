ccf9b9b8915f801cc61a6cfea81bb846435bb322
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-06-04 12:01:25 +0200

    [containers] use templates in MultiBitIterator

--- containers/MultiBitIterator.cpp
+++ containers/MultiBitIterator.cpp
@@ -17,59 +17,6 @@ m_bitOffset(0)
 
 MultiBitIterator::~MultiBitIterator() {}
 
-uint32_t MultiBitIterator::get32() const {
-	UByteArrayAdapter::OffsetType getPtr = m_data.tellGetPtr();
-	uint32_t res = static_cast<uint32_t>(m_data.getUint8(getPtr) << 8);
-	++getPtr;
-	uint8_t headerLength = 8-m_bitOffset;
-	int bitsRead = headerLength;
-	for(; bitsRead < 32 && getPtr < m_data.size(); bitsRead += 8) { 
-		uint8_t destLshift = (32-bitsRead > 8 ? 8 : 32-bitsRead);
-		uint8_t srcRShift = (destLshift < 8 ? 8-destLshift : 0);
-		uint8_t byte = m_data.getUint8(getPtr);
-		++getPtr;
-		byte >>= srcRShift;
-		res |= byte;
-		res <<= destLshift;
-	}
-	if (bitsRead < 32) {
-		res <<= (32-bitsRead);
-	}
-	
-	return res;
-}
-
-uint64_t MultiBitIterator::get64() const {
-	UByteArrayAdapter::OffsetType getPtr = m_data.tellGetPtr();
-	uint64_t res = m_data.getUint8(getPtr);
-	++getPtr;
-	uint8_t headerLength = 8-m_bitOffset;
-	int bitsRead = headerLength;
-	uint8_t destLshift = 8;
-	uint8_t srcRShift = 0;
-	for(; bitsRead < 64 && getPtr < m_data.size(); bitsRead += 8) {
-		destLshift = (64-bitsRead > 8 ? 8 : 64-bitsRead);
-		res <<= destLshift;
-		srcRShift = 8-destLshift;
-		uint8_t byte = m_data.getUint8(getPtr);
-		++getPtr;
-		byte >>= srcRShift;
-		res |= byte;
-	}
-	if (bitsRead < 64) {
-		res <<= (64-bitsRead);
-	}
-	return res;
-}
-
-uint32_t MultiBitIterator::get32(uint8_t len) const {
-	return get32() >> (32-len);
-}
-
-uint64_t MultiBitIterator::get64(uint8_t len) const {
-	return get64() >> (64-len);
-}
-
 MultiBitIterator & MultiBitIterator::operator+=(uint32_t bitCount) {
 	bitCount += m_bitOffset;
 	m_data.incGetPtr(bitCount/8);
--- include/sserialize/containers/MultiBitIterator.h
+++ include/sserialize/containers/MultiBitIterator.h
@@ -1,26 +1,60 @@
 #ifndef SSERIALIZE_MULTI_BIT_ITERATOR_H
 #define SSERIALIZE_MULTI_BIT_ITERATOR_H
 #include <sserialize/utility/UByteArrayAdapter.h>
+#include <limits>
 
 namespace sserialize {
 
 class MultiBitIterator {
 	UByteArrayAdapter m_data;
 	uint8_t m_bitOffset;
+private:
+	template<typename T_UINT_TYPE>
+	typename std::enable_if< std::is_unsigned<T_UINT_TYPE>::value, T_UINT_TYPE>::type getN() const {
+		UByteArrayAdapter::OffsetType getPtr = m_data.tellGetPtr();
+		T_UINT_TYPE res = static_cast<T_UINT_TYPE>(m_data.getUint8(getPtr)) << 8;
+		++getPtr;
+		uint8_t headerLength = 8-m_bitOffset;
+		int bitsRead = headerLength;
+		for(; bitsRead < std::numeric_limits<T_UINT_TYPE>::digits && getPtr < m_data.size(); bitsRead += 8) { 
+			uint8_t destLshift = (std::numeric_limits<T_UINT_TYPE>::digits-bitsRead > 8 ? 8 : std::numeric_limits<T_UINT_TYPE>::digits-bitsRead);
+			uint8_t srcRShift = (destLshift < 8 ? 8-destLshift : 0);
+			uint8_t byte = m_data.getUint8(getPtr);
+			++getPtr;
+			byte >>= srcRShift;
+			res |= byte;
+			res <<= destLshift;
+		}
+		if (bitsRead < std::numeric_limits<T_UINT_TYPE>::digits) {
+			res <<= (std::numeric_limits<T_UINT_TYPE>::digits-bitsRead);
+		}
+		return res;
+	}
+	
+	template<typename T_UINT_TYPE>
+	inline T_UINT_TYPE getN(uint8_t len) const {
+		return getN<T_UINT_TYPE>() >> (std::numeric_limits<T_UINT_TYPE>::digits-len);
+	}
+
+	
 public:
 	MultiBitIterator();
 	MultiBitIterator(const UByteArrayAdapter & data);
 	virtual ~MultiBitIterator();
-	uint32_t get32() const;
-	uint64_t get64() const;
+	inline uint16_t get16() const { return getN<uint16_t>(); }
+	inline uint32_t get32() const { return getN<uint32_t>(); }
+	inline uint64_t get64() const { return getN<uint64_t>(); }
+	///get the first @len bits with up to 16 bits
+	inline uint16_t get16(uint8_t len) const { return getN<uint16_t>(len);}
 	///get the first @len bits with up to 32 bits
-	uint32_t get32(uint8_t len) const;
+	inline uint32_t get32(uint8_t len) const { return getN<uint32_t>(len);}
 	///get the first @len bits with up to 64 bits
-	uint64_t get64(uint8_t len) const;
+	inline uint64_t get64(uint8_t len) const { return getN<uint64_t>(len);}
 	///@param bitCount this should not be larger than INT_MAX
 	MultiBitIterator & operator+=(uint32_t bitCount);
 	void reset();
 	bool hasNext() const;
+	UByteArrayAdapter::OffsetType dataSize() const { return m_data.size(); }
 };
 
 }//end namespace
