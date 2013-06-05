612e6dde53856fc35805247529048b13db99fce1
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-06-05 15:04:34 +0200

    [bench] improve necode bench

--- benchmarks/uint_dee_encode_bench.cpp
+++ benchmarks/uint_dee_encode_bench.cpp
@@ -1,21 +1,23 @@
 #include <sserialize/utility/UByteArrayAdapter.h>
 #include <sserialize/utility/TimeMeasuerer.h>
 #include <iostream>
+#include <set>
 
 std::vector<uint32_t> createNumbersSet(uint32_t count) {
-	std::set<uint32_t> ret;
 	//Fill the first
 	uint32_t rndNum;
 	uint32_t rndMask;
 	uint32_t mask;
-	while( ret.size() < count) {
+	std::vector<uint32_t> ret;
+	ret.reserve(count);
+	while(ret.size() < count) {
 		rndNum = rand();
 		rndMask = (double)rand()/RAND_MAX * 31; 
 		mask = ((rndMask+1 == 32) ? 0xFFFFFFFF : ((static_cast<uint32_t>(1) << (rndMask+1)) - 1));
 		uint32_t key = rndNum & mask;
-		ret.insert(key);
+		ret.push_back(key);
 	}
-	return std::vector<uint32_t>(ret.begin(), ret.end());
+	return ret;
 }
 
 int main(int argc, char ** argv) {
@@ -37,26 +39,54 @@ int main(int argc, char ** argv) {
 			uba.resize(testLength*4);
 			vec.reserve(testLength);
 			tm.begin();
-			for(uint32_t i = 0; i < testLength; ++i) {
-				uba.putUint32(nums[i]);
+			for(int i = 0; i < testLength; ++i) {
+				uba.putUint32(4*i, nums[i]);
 			}
 			uint32_t num = 0;
-			for(uint32_t i = 0; i < testLength; ++i) {
+			for(int i = 0; i < testLength; ++i) {
 				num += uba.getUint32(4*i);
 			}
 			tm.end();
 			std::cout << "UBA-Write-Read-Time: " << tm.elapsedMilliSeconds() << std::endl;
 			tm.begin();
-			for(uint32_t i = 0; i < testLength; ++i) {
+			for(int i = 0; i < testLength; ++i) {
 				vec.push_back(nums[i]);
 			}
-			uint32_t num = 0;
-			for(uint32_t i = 0; i < testLength; ++i) {
-				num += vec[i];
+			num = 0;
+			for(int i = 0; i < testLength; ++i) {
+				num += vec.at(i);
 			}
 			tm.end();
 			std::cout << "Vector-Write-Read-Time: " << tm.elapsedMilliSeconds() << std::endl;
 		}
 	}
 	
+	for(int testNum = 0; testNum < testCount; ++testNum) {
+		{
+			sserialize::UByteArrayAdapter uba(new std::vector<uint8_t>(), true);
+			std::vector<uint64_t> vec;
+			uba.resize(testLength*4);
+			vec.reserve(testLength);
+			tm.begin();
+			for(int i = 0; i < testLength; ++i) {
+				uba.putUint64(8*i, nums[i]);
+			}
+			uint32_t num = 0;
+			for(int i = 0; i < testLength; ++i) {
+				num += uba.getUint64(8*i);
+			}
+			tm.end();
+			std::cout << "UBA-Write-Read-Time: " << tm.elapsedMilliSeconds() << std::endl;
+			tm.begin();
+			for(int i = 0; i < testLength; ++i) {
+				vec.push_back(nums[i]);
+			}
+			num = 0;
+			for(int i = 0; i < testLength; ++i) {
+				num += vec.at(i);
+			}
+			tm.end();
+			std::cout << "Vector-Write-Read-Time: " << tm.elapsedMilliSeconds() << std::endl;
+		}
+	}
 }
