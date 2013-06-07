25eaa1ba92b9ffbc3cfb05a0ca571390b1459579
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-06-07 16:15:33 +0200

    [containers] StringCompleter that merges the result of multiple string completers into one

---
+++ completers/StringCompleterPrivateMulti.cpp
@@ -0,0 +1,64 @@
+#include <sserialize/completers/StringCompleterPrivateMulti.h>
+
+namespace sserialize {
+
+StringCompleterPrivateMulti::StringCompleterPrivateMulti() :
+StringCompleterPrivate(),
+m_sq(sserialize::StringCompleter::SQ_ALL)
+{}
+
+StringCompleterPrivateMulti::~StringCompleterPrivateMulti() {}
+
+
+void StringCompleterPrivateMulti::addCompleter(const RCPtrWrapper<sserialize::StringCompleterPrivate> & completer) {
+	m_sq = (sserialize::StringCompleter::SupportedQuerries)(m_sq & completer->getSupportedQuerries());
+}
+
+ItemIndex StringCompleterPrivateMulti::complete(const std::string & str, StringCompleter::QuerryType qtype) const {
+	std::vector<ItemIndex> idxs;
+	idxs.reserve(m_completers.size());
+	ItemIndex idx;
+	for(std::size_t i = 0; i < m_completers.size(); ++i) {
+		idx = m_completers[i]->complete(str, qtype);
+		if (idx.size())
+			idxs.push_back(idx);
+	}
+	return ItemIndex::unite(idxs);
+}
+
+ItemIndexIterator StringCompleterPrivateMulti::partialComplete(const std::string & str, sserialize::StringCompleter::QuerryType qtype) const {
+	std::vector<ItemIndexIterator> idxs;
+	idxs.reserve(m_completers.size());
+	ItemIndexIterator idx;
+	for(std::size_t i = 0; i < m_completers.size(); ++i) {
+		idx = m_completers[i]->partialComplete(str, qtype);
+		if (idx.maxSize())
+			idxs.push_back(idx);
+	}
+	return ItemIndexIterator::createTree(idxs.begin(), idxs.end(), &createMergeItemIndexIterator);
+}
+
+StringCompleter::SupportedQuerries StringCompleterPrivateMulti::getSupportedQuerries() const {
+	return m_sq;
+}
+
+std::ostream& StringCompleterPrivateMulti::printStats(std::ostream& out) const {
+	out << "StringCompleterPrivateMulti::BEGIN" << std::endl;
+	for(std::size_t i = 0; i < m_completers.size(); ++i) {
+		out << m_completers[i]->printStats(out) << std::endl;
+	}
+	out << "StringCompleterPrivateMulti::END" << std::endl;
+	return out;
+}
+
+std::string StringCompleterPrivateMulti::getName() const {
+	std::string myName("StringCompleterPrivateMulti: ");
+	for(std::size_t i = 0; i < m_completers.size(); ++i) {
+		myName += m_completers[i]->getName();
+		myName += ":";
+	}
+	return myName;
+}
+
+
+}//end namespace
---
+++ include/sserialize/completers/StringCompleterPrivateMulti.h
@@ -0,0 +1,31 @@
+#ifndef SSERIALIZE_STRING_COMPLETER_PRIVATE_MULTI_H
+#define SSERIALIZE_STRING_COMPLETER_PRIVATE_MULTI_H
+#include <sserialize/completers/StringCompleterPrivate.h>
+
+namespace sserialize {
+
+class StringCompleterPrivateMulti: public StringCompleterPrivate {
+private:
+	StringCompleter::SupportedQuerries m_sq;
+	std::vector< RCPtrWrapper<sserialize::StringCompleterPrivate> > m_completers;
+public:
+	StringCompleterPrivateMulti();
+	virtual ~StringCompleterPrivateMulti();
+	
+	void addCompleter(const RCPtrWrapper<sserialize::StringCompleterPrivate> & completer);
+
+	virtual ItemIndex complete(const std::string & str, StringCompleter::QuerryType qtype) const;
+	
+	virtual ItemIndexIterator partialComplete(const std::string & str, sserialize::StringCompleter::QuerryType qtype) const;
+	
+	virtual StringCompleter::SupportedQuerries getSupportedQuerries() const;
+	virtual std::ostream& printStats(std::ostream& out) const;
+	
+	virtual std::string getName() const;
+
+
+};
+
+}//end namespace
+
+#endif
