315285c2c612ce91e92d1f0ed6f8a76c7163e1ca
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-06-10 14:48:27 +0200

    [containres] UDWConstrainedIterator

---
+++ include/sserialize/containers/UDWConstrainedIterator.h
@@ -0,0 +1,44 @@
+#ifndef SSERIALIZE_UDW_CONSTRAINED_ITERATOR_H
+#define SSERIALIZE_UDW_CONSTRAINED_ITERATOR_H
+#include <sserialize/containers/UDWIterator.h>
+
+namespace sserialize {
+
+class UDWConstrainedIterator: public RCWrapper<UDWIteratorPrivate> {
+	typedef RCWrapper<UDWIteratorPrivate> MyBaseClass;
+private:
+	uint32_t m_size;
+	uint32_t m_curOffset;
+public:
+	///creates a direct UDW Iterator
+	UDWConstrainedIterator(const UByteArrayAdapter & data, uint32_t size)
+	: MyBaseClass(new UDWIteratorPrivateDirect(data)), m_size(size), m_curOffset(0) {}
+	UDWConstrainedIterator(UDWIteratorPrivate *  priv, uint32_t size) : MyBaseClass(priv), m_size(size), m_curOffset(0) {}
+	UDWConstrainedIterator(const UDWConstrainedIterator & other) : MyBaseClass(other), m_size(other.m_size), m_curOffset(other.m_curOffset) {}
+	UDWConstrainedIterator() : MyBaseClass(new UDWIteratorPrivate() ), m_size(0), m_curOffset(0) {}
+	virtual ~UDWConstrainedIterator() {}
+	UDWConstrainedIterator & operator=(const UDWConstrainedIterator & other) {
+		MyBaseClass::operator=(other);
+		m_size = other.m_size;
+		m_curOffset = other.m_curOffset;
+		return *this;
+	}
+	inline uint32_t next() {
+		++m_curOffset;
+		return priv()->next();
+	}
+	inline bool hasNext() {
+		return m_curOffset < m_size;
+	}
+	UByteArrayAdapter::OffsetType dataSize() const {
+		return priv()->dataSize();
+	}
+	void reset() {
+		priv()->reset();
+		m_curOffset = 0;
+	}
+};
+
+}//end namespace
+
+#endif
