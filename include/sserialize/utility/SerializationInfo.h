43b5e0efc191873cdabb64f3a2b4cfdc5db36256
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-06-17 17:28:57 +0200

    [utility] fix SerializationInfo

--- include/sserialize/utility/SerializationInfo.h
+++ include/sserialize/utility/SerializationInfo.h
@@ -15,29 +15,29 @@ struct SerializationInfo {
 //default overloads
 
 //uint8_t
-template<> SerializationInfo<uint8_t>::is_fixed_length() { return true; }
-template<> SerializationInfo<uint8_t>::length() { return 1; }
-template<> SerializationInfo<uint8_t>::max_length() { return 1; }
-template<> SerializationInfo<uint8_t>::min_length() { return 1; }
+template<> inline bool SerializationInfo<uint8_t>::is_fixed_length() { return true; }
+template<> inline OffsetType SerializationInfo<uint8_t>::length() { return 1; }
+template<> inline OffsetType SerializationInfo<uint8_t>::max_length() { return 1; }
+template<> inline OffsetType SerializationInfo<uint8_t>::min_length() { return 1; }
 
 //uint16_t
-template<> SerializationInfo<uint16_t>::is_fixed_length() { return true; }
-template<> SerializationInfo<uint16_t>::length() { return 2; }
-template<> SerializationInfo<uint16_t>::max_length() { return 2; }
-template<> SerializationInfo<uint16_t>::min_length() { return 2; }
+template<> inline bool SerializationInfo<uint16_t>::is_fixed_length() { return true; }
+template<> inline OffsetType SerializationInfo<uint16_t>::length() { return 2; }
+template<> inline OffsetType SerializationInfo<uint16_t>::max_length() { return 2; }
+template<> inline OffsetType SerializationInfo<uint16_t>::min_length() { return 2; }
 
 
 //uint32_t
-template<> SerializationInfo<uint32_t>::is_fixed_length() { return true; }
-template<> SerializationInfo<uint32_t>::length() { return 4; }
-template<> SerializationInfo<uint32_t>::max_length() { return 4; }
-template<> SerializationInfo<uint32_t>::min_length() { return 4; }
+template<> inline bool SerializationInfo<uint32_t>::is_fixed_length() { return true; }
+template<> inline OffsetType SerializationInfo<uint32_t>::length() { return 4; }
+template<> inline OffsetType SerializationInfo<uint32_t>::max_length() { return 4; }
+template<> inline OffsetType SerializationInfo<uint32_t>::min_length() { return 4; }
 
 //uint64_t
-template<> SerializationInfo<uint64_t>::is_fixed_length() { return true; }
-template<> SerializationInfo<uint64_t>::length() { return 8; }
-template<> SerializationInfo<uint64_t>::max_length() { return 8; }
-template<> SerializationInfo<uint64_t>::min_length() { return 8; }
+template<> inline bool SerializationInfo<uint64_t>::is_fixed_length() { return true; }
+template<> inline OffsetType SerializationInfo<uint64_t>::length() { return 8; }
+template<> inline OffsetType SerializationInfo<uint64_t>::max_length() { return 8; }
+template<> inline OffsetType SerializationInfo<uint64_t>::min_length() { return 8; }
 
 }//end namespace
 
