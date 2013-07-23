7248ed2dcc12e3753de9417efcb5fe8230d97a5e
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-07-23 10:50:18 +0200

    [util] improve AtStlInputIterator

--- include/sserialize/utility/AtStlInputIterator.h
+++ include/sserialize/utility/AtStlInputIterator.h
@@ -6,20 +6,33 @@
 
 namespace sserialize {
 
+template<typename T_CONTAINER, typename T_RETURN_TYPE, typename T_SIZE_TYPE>
+struct ReadOnlyAtStlIteratorPassThroughDereference {
+
+	T_RETURN_TYPE operator()(const T_CONTAINER & c, std::enable_if<std::is_pointer<T_CONTAINER>::value, T_SIZE_TYPE>  pos) {
+		return c->at(pos);
+	}
+	
+	T_RETURN_TYPE operator()(const T_CONTAINER & c, std::enable_if<!std::is_pointer<T_CONTAINER>::value, T_SIZE_TYPE>  pos) {
+		return c.at(pos);
+	}
+};
+
 /** This is a template class to iterate over a container which element access function is named at(size_t)
   *
   *
   *
   */
-
-template<typename T_CONTAINER, typename T_RETURN_TYPE, typename T_SIZE_TYPE=std::size_t>
+template<typename T_CONTAINER, typename T_RETURN_TYPE, typename T_SIZE_TYPE=std::size_t, typename T_DEREFERENCE = ReadOnlyAtStlIteratorPassThroughDereference<T_CONTAINER, T_RETURN_TYPE, T_SIZE_TYPE> >
 class ReadOnlyAtStlIterator: public std::iterator<std::random_access_iterator_tag, T_RETURN_TYPE, T_SIZE_TYPE> {
 private:
 	T_SIZE_TYPE m_pos;
 	T_CONTAINER m_data;
+	T_DEREFERENCE m_derefer;
 public:
 	ReadOnlyAtStlIterator() : m_pos(), m_data() {}
 	ReadOnlyAtStlIterator(T_SIZE_TYPE pos, T_CONTAINER data) : m_pos(pos), m_data(data) {}
+	ReadOnlyAtStlIterator(T_SIZE_TYPE pos, T_CONTAINER data, T_DEREFERENCE derefer) : m_pos(pos), m_data(data), m_derefer(derefer) {}
 	virtual ~ReadOnlyAtStlIterator() {}
 	T_SIZE_TYPE & pos() { return m_pos;}
 	const T_SIZE_TYPE & pos() const { return m_pos;}
@@ -83,18 +96,12 @@ public:
 	}
 
 	T_SIZE_TYPE operator-(const ReadOnlyAtStlIterator & other) {
-		return (m_data == other ? m_pos - other.m_pos : std::numeric_limits<T_SIZE_TYPE>::max());
+		return (m_data == other.m_data ? m_pos - other.m_pos : std::numeric_limits<T_SIZE_TYPE>::max());
 	}
 	
 	T_RETURN_TYPE operator*() {
-		if (std::is_pointer<T_CONTAINER>::value) {
-			return m_data->at(m_pos);
-		}
-		else {
-			return m_data.at(m_pos);
-		}
+		return m_derefer(m_data, m_pos);
 	}
-	
 };
 
 }//end namespace
