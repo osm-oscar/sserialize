53250a4bd5ed995e8700822f9a099b86132a4b52
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-07-23 10:48:11 +0200

    [spatial] split GeoRect into header/implementation

--- include/sserialize/spatial/GeoRect.h
+++ include/sserialize/spatial/GeoRect.h
@@ -10,90 +10,37 @@ class GeoRect {
 	double m_lat[2];
 	double m_lon[2];
 public:
-	GeoRect() {
-		m_lat[0] = 0;
-		m_lat[1] = 0;
-		m_lon[0] = 0;
-		m_lon[1] = 0;
-	}
+	GeoRect();
+	GeoRect(const GeoRect & other);
+	GeoRect(double latLeft, double latRight, double lonLeft, double lonRight);
+	virtual ~GeoRect();
+	double* lat();
+	const double* lat() const;
+	double* lon();
+	const double* lon() const;
 	
-	GeoRect(const GeoRect & other) : m_lat({other.m_lat[0], other.m_lat[1]}), m_lon({other.m_lon[0], other.m_lon[1]}) {}
+	double minLat() const;
+	double maxLat() const;
 	
-	GeoRect(double latLeft, double latRight, double lonLeft, double lonRight) {
-		if (latLeft > latRight)
-			std::swap(latLeft, latRight);
-		if (lonLeft > lonRight)
-			std::swap(lonLeft, lonRight);
-		m_lat[0] = latLeft;
-		m_lat[1] = latRight;
-		m_lon[0] = lonLeft;
-		m_lon[1] = lonRight;
-	}
-	virtual ~GeoRect() {}
-	double* lat() { return m_lat; }
-	const double* lat() const { return m_lat; }
-	double* lon() { return m_lon; }
-	const double* lon() const { return m_lon; }
+	double minLon() const;
+	double maxLon() const;
 	
-	double minLat() const { return m_lat[0]; }
-	double maxLat() const { return m_lat[1]; }
+	bool overlap(const GeoRect & other) const;
+	bool contains(double lat, double lon) const;
 	
-	double minLon() const { return m_lon[0]; }
-	double maxLon() const { return m_lon[1]; }
+	bool contains(const GeoRect & other) const;
 	
-	bool overlap(const GeoRect & other) const {
-		if ((m_lat[0] > other.m_lat[1]) || // this is left of other
-			(m_lat[1] < other.m_lat[0]) || // this is right of other
-			(m_lon[0] > other.m_lon[1]) ||
-			(m_lon[1] < other.m_lon[0])) {
-				return false;
-		}
-		return true;
-	}
-	bool contains(double lat, double lon) const {
-		return (m_lat[0] <= lat && lat <= m_lat[1] && m_lon[0] <= lon && lon <= m_lon[1]);
-	}
-	
-	bool contains(const GeoRect & other) const {
-		return contains(other.m_lat[0], other.m_lon[0]) && contains(other.m_lat[1], other.m_lon[1]);
-	}
-	
-	bool operator==(const GeoRect & other) const {
-		for(size_t i = 0; i < 2; i++) {
-			if (m_lat[i] != other.m_lat[i] || m_lon[i] != other.m_lon[i])
-				return false;
-		}
-		return true;
-	}
-	
+	bool operator==(const GeoRect & other) const;
 	/** clip this rect at other
 	  * @return: returns true if clipping worked => rects overlapped, false if they did not overlap */
-	bool clip(const GeoRect & other) {
-		if (!overlap(other))
-			return false;
-		m_lat[0] = std::max(m_lat[0], other.m_lat[0]);
-		m_lat[1] = std::min(m_lat[1], other.m_lat[1]);
-		
-		m_lon[0] = std::max(m_lon[0], other.m_lon[0]);
-		m_lon[1] = std::min(m_lon[1], other.m_lon[1]);
-		
-		return true;
-	}
+	bool clip(const GeoRect & other);
 	
 	/** Enlarge this rect so that other will fit into it */
-	void enlarge(const GeoRect & other) {
-		m_lat[0] = std::min(m_lat[0], other.m_lat[0]);
-		m_lat[1] = std::max(m_lat[1], other.m_lat[1]);
-		
-		m_lon[0] = std::min(m_lon[0], other.m_lon[0]);
-		m_lon[1] = std::max(m_lon[1], other.m_lon[1]);
-	}
+	void enlarge(const GeoRect & other);
 };
 
 }}//end namespace
 
-inline std::ostream & operator<<(std::ostream & out, const sserialize::spatial::GeoRect & rect) {
-	return out << "GeoRect[(" << rect.lat()[0] << ", " << rect.lon()[0] << "); (" << rect.lat()[1] << ", " << rect.lon()[1]  << ")]";
-}
+std::ostream & operator<<(std::ostream & out, const sserialize::spatial::GeoRect & rect);
 
 #endif
---
+++ spatial/GeoRect.cpp
@@ -0,0 +1,91 @@
+#include <sserialize/spatial/GeoRect.h>
+
+namespace sserialize {
+namespace spatial {
+
+
+GeoRect::GeoRect() {
+	m_lat[0] = 0;
+	m_lat[1] = 0;
+	m_lon[0] = 0;
+	m_lon[1] = 0;
+}
+
+GeoRect::GeoRect(const GeoRect & other) : m_lat({other.m_lat[0], other.m_lat[1]}), m_lon({other.m_lon[0], other.m_lon[1]}) {}
+
+GeoRect::GeoRect(double latLeft, double latRight, double lonLeft, double lonRight) {
+	if (latLeft > latRight)
+		std::swap(latLeft, latRight);
+	if (lonLeft > lonRight)
+		std::swap(lonLeft, lonRight);
+	m_lat[0] = latLeft;
+	m_lat[1] = latRight;
+	m_lon[0] = lonLeft;
+	m_lon[1] = lonRight;
+}
+GeoRect::~GeoRect() {}
+double* GeoRect::lat() { return m_lat; }
+const double* GeoRect::lat() const { return m_lat; }
+double* GeoRect::lon() { return m_lon; }
+const double* GeoRect::lon() const { return m_lon; }
+
+double GeoRect::minLat() const { return m_lat[0]; }
+double GeoRect::maxLat() const { return m_lat[1]; }
+
+double GeoRect::minLon() const { return m_lon[0]; }
+double GeoRect::maxLon() const { return m_lon[1]; }
+
+bool GeoRect::overlap(const GeoRect & other) const {
+	if ((m_lat[0] > other.m_lat[1]) || // this is left of other
+		(m_lat[1] < other.m_lat[0]) || // this is right of other
+		(m_lon[0] > other.m_lon[1]) ||
+		(m_lon[1] < other.m_lon[0])) {
+			return false;
+	}
+	return true;
+}
+
+bool GeoRect::contains(double lat, double lon) const {
+	return (m_lat[0] <= lat && lat <= m_lat[1] && m_lon[0] <= lon && lon <= m_lon[1]);
+}
+
+bool GeoRect::contains(const GeoRect & other) const {
+	return contains(other.m_lat[0], other.m_lon[0]) && contains(other.m_lat[1], other.m_lon[1]);
+}
+
+bool GeoRect::operator==(const GeoRect & other) const {
+	for(size_t i = 0; i < 2; i++) {
+		if (m_lat[i] != other.m_lat[i] || m_lon[i] != other.m_lon[i])
+			return false;
+	}
+	return true;
+}
+
+/** clip this rect at other
+	* @return: returns true if clipping worked => rects overlapped, false if they did not overlap */
+bool GeoRect::clip(const GeoRect & other) {
+	if (!overlap(other))
+		return false;
+	m_lat[0] = std::max(m_lat[0], other.m_lat[0]);
+	m_lat[1] = std::min(m_lat[1], other.m_lat[1]);
+	
+	m_lon[0] = std::max(m_lon[0], other.m_lon[0]);
+	m_lon[1] = std::min(m_lon[1], other.m_lon[1]);
+	
+	return true;
+}
+
+/** Enlarge this rect so that other will fit into it */
+void GeoRect::enlarge(const GeoRect & other) {
+	m_lat[0] = std::min(m_lat[0], other.m_lat[0]);
+	m_lat[1] = std::max(m_lat[1], other.m_lat[1]);
+	
+	m_lon[0] = std::min(m_lon[0], other.m_lon[0]);
+	m_lon[1] = std::max(m_lon[1], other.m_lon[1]);
+}
+
+}}//end namespace
+
+std::ostream & operator<<(std::ostream & out, const sserialize::spatial::GeoRect & rect) {
+	return out << "GeoRect[(" << rect.lat()[0] << ", " << rect.lon()[0] << "); (" << rect.lat()[1] << ", " << rect.lon()[1]  << ")]";
+}
