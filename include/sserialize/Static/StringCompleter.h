e1c62b14995e67f855501e83c88e1e4dd5f94683
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-07-25 14:04:45 +0200

    [Static] add conineince functions to TO CREATE STRINGC OMPLETERS

--- include/sserialize/Static/StringCompleter.h
+++ include/sserialize/Static/StringCompleter.h
@@ -2,6 +2,12 @@
 #define SSERIALIZE_STATIC_STRING_COMPLETER_H
 #include <deque>
 #include <sserialize/completers/StringCompleterPrivate.h>
+#include <sserialize/Static/GeneralizedTrie.h>
+#include <sserialize/Static/FlatGeneralizedTrie.h>
+#include <sserialize/Static/FlatGSTStrIds.h>
+#include <sserialize/completers/StringCompleter.h>
+#include <sserialize/completers/StringCompleterPrivateStaticDB.h>
+#include <sserialize/Static/ItemIndexStore.h>
 
 #define STATIC_STRING_COMPLETER_HEADER_SIZE 2
 
@@ -28,6 +34,26 @@ struct StringCompleter {
 		destination.push_back(0);
 		destination.push_back(type);
 		return true;
+	}
+	
+	template<typename DataBaseType>
+	static sserialize::StringCompleter fromDB(const DataBaseType & db) {
+		return sserialize::StringCompleter(new StringCompleterPrivateStaticDB<DataBaseType>(db));
+	}
+	
+	template<typename DataBaseType>
+	static sserialize::StringCompleter fromData(const UByteArrayAdapter & data, const sserialize::Static::ItemIndexStore & indexStore, const DataBaseType & db) {
+		uint8_t type = data[1];
+		switch (type) {
+		case (Static::StringCompleter::T_TRIE):
+			return sserialize::StringCompleter(new Static::GeneralizedTrie(data+2, indexStore));
+		case (Static::StringCompleter::T_FLAT_TRIE):
+			return sserialize::StringCompleter(new Static::FlatGST (data+2, indexStore));
+		case (Static::StringCompleter::T_FLAT_TRIE_STRIDS):
+			return sserialize::StringCompleter(new sserialize::Static::FlatGSTStrIds<DataBaseType>( data+2, indexStore, db) );
+		default:
+			return sserialize::StringCompleter();
+		}
 	}
 };
 
