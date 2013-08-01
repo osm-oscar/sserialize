cc4ab5908b8ef1cd778f169a97912b5f6c780aec
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-08-01 11:02:07 +0200

    [containers] initial draft to support the creation of a staic trie directly from data

--- include/sserialize/containers/GeneralizedTrie.h
+++ include/sserialize/containers/GeneralizedTrie.h
@@ -4,6 +4,7 @@
 #include <sserialize/containers/StringsItemDBWrapper.h>
 #include <sserialize/utility/utilfuncs.h>
 #include <sserialize/templated/StringsItemDBWrapperPrivateSIDB.h>
+#include <sserialize/templated/WindowedArray.h>
 #include <sserialize/utility/UByteArrayAdapter.h>
 #include <sserialize/Static/FlatGeneralizedTrie.h>
 #include <sserialize/utility/debuggerfunctions.h>
@@ -205,6 +206,9 @@ public: //Fill functions
 	  */
 	template<typename T_ITEM_FACTORY, typename T_ITEM>
 	bool fromStringsFactory(const T_ITEM_FACTORY & stringsFactor);
+
+	template<typename T_ITEM_FACTORY, typename T_ITEM>
+	bool staticFromStringsFactory(const T_ITEM_FACTORY & stringsFactory, sserialize::UByteArrayAdapter & dest);
 
 public://Completion
 	ItemIndex complete(const std::string & str, sserialize::StringCompleter::QuerryType qt = sserialize::StringCompleter::QT_SUFFIX_PREFIX) const;
@@ -607,9 +611,93 @@ bool GeneralizedTrie<ItemType>::fromStringsFactory(const T_ITEM_FACTORY & string
 
 	return true;
 }
-	
-	
 
+template<class ItemType>
+template<typename T_ITEM_FACTORY, typename T_ITEM>
+bool GeneralizedTrie<ItemType>::staticFromStringsFactory(const T_ITEM_FACTORY & stringsFactory, UByteArrayAdapter & dest) {
+	std::unordered_map<std::string, std::unordered_set<Node*> > strIdToSubStrNodes;
+	std::unordered_map<std::string, std::unordered_set<Node*> > strIdToExactNodes;
+	uint64_t exactStorageNeed, suffixStorageNeed;
+	
+	if (trieFromStringsFactory(stringsFactory, strIdToSubStrNodes, strIdToExactNodes)) {
+		return false;
+	}
+	
+	//Now add the items
+	ProgressInfo progressInfo;
+	progressInfo.begin(stringsFactory.end()-stringsFactory.begin(), "GeneralizedTrie::fromStringsFactory::calculating storage need");
+	uint32_t count = 0;
+	for(typename T_ITEM_FACTORY::const_iterator itemIt(stringsFactory.begin()), itemEnd(stringsFactory.end()); itemIt != itemEnd; ++itemIt) {
+		std::unordered_set<Node*> exactNodes;
+		std::unordered_set<Node*> suffixNodes;
+		T_ITEM item = *itemIt;
+		for(typename T_ITEM::const_iterator itemStrsIt(item.begin()), itemStrsEnd(item.end()); itemStrsIt != itemStrsEnd; ++itemStrsIt) {
+			if (strIdToExactNodes.count(*itemStrsIt)) {
+				exactNodes.insert(strIdToExactNodes[*itemStrsIt].begin(), strIdToExactNodes[*itemStrsIt].end());
+			}
+			else {
+				std::cout << "ERROR: No exact node for item string" << std::endl;
+			}
+			if (strIdToSubStrNodes.count(*itemStrsIt)) {
+				suffixNodes.insert(strIdToSubStrNodes[*itemStrsIt].begin(), strIdToSubStrNodes[*itemStrsIt].end());
+			}
+		}
+		
+		exactStorageNeed += exactNodes.size();
+		suffixStorageNeed += suffixNodes.size();
+
+		for(std::unordered_set<Node*>::iterator esit = exactNodes.begin(); esit != exactNodes.end(); ++esit) {
+			(*esit)->exactValues.reserve((*esit)->exactValues.capacity()+1);
+		}
+		for(std::unordered_set<Node*>::iterator it = suffixNodes.begin(); it != suffixNodes.end(); ++it) {
+			(*it)->subStrValues.reserve((*it)->subStrValues.capacity()+1);
+		}
+
+
+		progressInfo(++count);
+	}
+	progressInfo.end();
+	std::cout << std::endl;
+	
+	progressInfo.begin(stringsFactory.end()-stringsFactory.begin(), "GeneralizedTrie::fromStringsFactory::popluating exact/suffix index");
+	uint32_t count = 0;
+	for(typename T_ITEM_FACTORY::const_iterator itemIt(stringsFactory.begin()), itemEnd(stringsFactory.end()); itemIt != itemEnd; ++itemIt) {
+		std::unordered_set<Node*> exactNodes;
+		std::unordered_set<Node*> suffixNodes;
+		T_ITEM item = *itemIt;
+		for(typename T_ITEM::const_iterator itemStrsIt(item.begin()), itemStrsEnd(item.end()); itemStrsIt != itemStrsEnd; ++itemStrsIt) {
+			if (strIdToExactNodes.count(*itemStrsIt)) {
+				exactNodes.insert(strIdToExactNodes[*itemStrsIt].begin(), strIdToExactNodes[*itemStrsIt].end());
+			}
+			else {
+				std::cout << "ERROR: No exact node for item string" << std::endl;
+			}
+			if (strIdToSubStrNodes.count(*itemStrsIt)) {
+				suffixNodes.insert(strIdToSubStrNodes[*itemStrsIt].begin(), strIdToSubStrNodes[*itemStrsIt].end());
+			}
+		}
+		
+		for(std::unordered_set<Node*>::iterator esit = exactNodes.begin(); esit != exactNodes.end(); ++esit) {
+			(*esit)->exactValues.insert((*esit)->exactValues.end(), count);
+		}
+		for(std::unordered_set<Node*>::iterator it = suffixNodes.begin(); it != suffixNodes.end(); ++it) {
+			(*it)->subStrValues.insert((*it)->subStrValues.end(), count);
+		}
+
+		progressInfo(++count);
+	}
+	progressInfo.end();
+	
+	if (!consistencyCheck()) {
+		std::cout << "Trie is broken after GeneralizedTrie::fromStringsFactory::insertItems" << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+	
+	
 /** This function sets the DB the trie uses and creates the trie out of the DB data
   * It does so in 3 Steps:
   * 1. Create trie nodes with all the strings in the db's string table
