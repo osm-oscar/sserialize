7eb971adb78b68b9db21e92d3489df63c117715a
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-08-06 14:37:15 +0200

    [Static] DynamicVector::reserve()

--- include/sserialize/Static/DynamicVector.h
+++ include/sserialize/Static/DynamicVector.h
@@ -15,6 +15,7 @@ public:
 	DynamicVector(uint32_t approxItemCount, OffsetType initalDataSize);
 	virtual ~DynamicVector();
 	uint32_t size() const;
+	void reserve(uint32_t size) {}
 	template<typename TStreamingSerializer = UByteArrayAdapter::StreamingSerializer<TPushValue> >
 	void push_back(const TPushValue & value, const TStreamingSerializer & serializer = TStreamingSerializer());
 	void pop_back();
