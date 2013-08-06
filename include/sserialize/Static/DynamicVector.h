e60aafb95d2532d9d64911edf75c67dab50e4c49
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-08-06 12:12:18 +0200

    [Static] DynamicVector

---
+++ include/sserialize/Static/DynamicVector.h
@@ -0,0 +1,102 @@
+#ifndef SSERIALIZE_DYNAMIC_VECTOR_H
+#define SSERIALIZE_DYNAMIC_VECTOR_H
+#include <sserialize/utility/UByteArrayAdapter.h>
+
+namespace sserialize {
+namespace Static {
+
+template<typename TPushValue, typename TGetValue = TPushValue>
+class DynamicVector {
+public:
+	uint32_t m_size;
+	UByteArrayAdapter m_offsets;
+	UByteArrayAdapter m_data;
+public:
+	DynamicVector(uint32_t approxItemCount, OffsetType initalDataSize);
+	virtual ~DynamicVector();
+	uint32_t size() const;
+	template<typename TStreamingSerializer = UByteArrayAdapter::StreamingSerializer<TPushValue> >
+	void push_back(const TPushValue & value, const TStreamingSerializer & serializer = TStreamingSerializer());
+	void pop_back();
+	UByteArrayAdapter & beginRawPush();
+	void endRawPush();
+	template<typename TDeserializer = UByteArrayAdapter::Deserializer<TGetValue> >
+	TGetValue at(uint32_t pos, const TDeserializer & derefer = TDeserializer()) const;
+	UByteArrayAdapter dataAt(uint32_t pos) const;
+};
+
+template<typename TPushValue, typename TGetValue>
+DynamicVector<TPushValue, TGetValue>::DynamicVector(uint32_t approxItemCount, OffsetType initalDataSize) :
+m_size(0),
+m_offsets(UByteArrayAdapter::createCache(approxItemCount*UByteArrayAdapter::OffsetTypeSerializedLength(), true)),
+m_data(UByteArrayAdapter::createCache(initalDataSize, true))
+{}
+
+
+template<typename TPushValue, typename TGetValue>
+DynamicVector<TPushValue, TGetValue>::~DynamicVector() {}
+
+template<typename TPushValue, typename TGetValue>
+uint32_t DynamicVector<TPushValue, TGetValue>::size() const {
+	return m_size;
+}
+
+template<typename TPushValue, typename TGetValue>
+template<typename TStreamingSerializer>
+void DynamicVector<TPushValue, TGetValue>::push_back(const TPushValue & value, const TStreamingSerializer & serializer) {
+	serializer.operator()(value, beginRawPush());
+	endRawPush();
+}
+
+template<typename TPushValue, typename TGetValue>
+void DynamicVector<TPushValue, TGetValue>::pop_back() {
+	if (m_size) {
+		OffsetType tmp = m_offsets.getOffset((m_size-1)*UByteArrayAdapter::OffsetTypeSerializedLength());
+		m_offsets.setPutPtr((m_size-1)*UByteArrayAdapter::OffsetTypeSerializedLength());
+		--m_size;
+		if (!m_size) {
+			m_data.setPutPtr(0);
+		}
+		else {
+			m_data.setPutPtr(tmp);
+		}
+	}
+}
+
+template<typename TPushValue, typename TGetValue>
+UByteArrayAdapter & DynamicVector<TPushValue, TGetValue>::beginRawPush() {
+	m_offsets.putOffset(m_data.tellPutPtr());
+	return m_data;
+}
+
+template<typename TPushValue, typename TGetValue>
+void DynamicVector<TPushValue, TGetValue>::endRawPush() {
+	++m_size;
+}
+
+template<typename TPushValue, typename TGetValue>
+template<typename TDeserializer>
+TGetValue DynamicVector<TPushValue, TGetValue>::at(uint32_t pos, const TDeserializer & deserializer) const {
+	if (pos >= size())
+		return TGetValue();
+	return deserializer(dataAt(pos));
+}
+
+template<typename TPushValue, typename TGetValue>
+UByteArrayAdapter DynamicVector<TPushValue, TGetValue>::dataAt(uint32_t pos) const {
+	if (pos >= size())
+		return UByteArrayAdapter();
+	OffsetType begin = m_offsets.getOffset(pos*UByteArrayAdapter::OffsetTypeSerializedLength());
+	OffsetType len;
+	if (pos == m_size-1) {
+		len = m_data.tellPutPtr()-begin;
+	}
+	else {
+		len = m_offsets.getOffset((pos+1)*UByteArrayAdapter::OffsetTypeSerializedLength())-begin;
+	}
+	return UByteArrayAdapter(m_data, begin, len);
+}
+
+}}//end namespace
+
+#endif
---
+++ tests/static_DynamicVector.cpp
@@ -0,0 +1,90 @@
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include <sserialize/Static/DynamicVector.h>
+#include <sserialize/utility/log.h>
+#include "datacreationfuncs.h"
+
+using namespace sserialize;
+
+struct ComplexData {
+	ComplexData() : str1(createString(40)), str2(createString(20)), num1(rand()), num2(rand()) {} 
+	std::string str1;
+	std::string str2;
+	uint32_t num1;
+	uint64_t num2;
+};
+
+bool operator==(const ComplexData & a, const ComplexData & b) {
+	return (a.str1 == b.str1) && (a.str2 == b.str2) && (a.num1 == b.num1) && (a.num2 == b.num2);
+}
+
+std::ostream & operator<<(std::ostream & out, const ComplexData & a) {
+	out << "ComplexData[" << a.str1 << ", " << a.str2 << ", " << a.num1 << ", " << a.num2 << "]";
+	return out;
+}
+
+struct ComplexDataSerializer {
+	void operator()(const ComplexData & src, sserialize::UByteArrayAdapter & dest) const {
+		dest << src.str1 << src.str2 << src.num1 << src.num2;
+	}
+};
+
+struct ComplexDataDeserializer {
+	ComplexData operator()(const UByteArrayAdapter & data) const {
+		UByteArrayAdapter dd = data;
+		ComplexData d;
+		dd >> d.str1 >> d.str2 >> d.num1 >> d.num2;
+		return d;
+	};
+};
+
+
+template<int NumberOfItems>
+class DynamicVectorTest: public CppUnit::TestFixture {
+CPPUNIT_TEST_SUITE( DynamicVectorTest );
+CPPUNIT_TEST( testDefaultSerializer );
+CPPUNIT_TEST( testComplexSerialize );
+CPPUNIT_TEST_SUITE_END();
+public:
+	virtual void setUp() {}
+	virtual void tearDown() {}
+	void testDefaultSerializer() {
+		std::deque<uint32_t> rawData = sserialize::createNumbers(NumberOfItems);
+		sserialize::Static::DynamicVector<uint32_t, uint32_t> dynVec(NumberOfItems, NumberOfItems*sserialize::SerializationInfo<uint32_t>::length);
+		for(uint32_t i = 0, s = rawData.size(); i < s; ++i) {
+			dynVec.push_back(rawData[i]);
+		}
+		
+		CPPUNIT_ASSERT_EQUAL_MESSAGE("size does not match", (uint32_t)rawData.size(), dynVec.size());
+		
+		for(uint32_t i = 0, s = rawData.size(); i < s; ++i) {
+			CPPUNIT_ASSERT_EQUAL_MESSAGE(osmfindlog::toString("item at ", i), rawData[i], dynVec.at(i));
+		}
+	}
+	
+	void testComplexSerialize() {
+		std::vector<ComplexData> rawData(NumberOfItems);
+		sserialize::Static::DynamicVector<ComplexData> dynVec(NumberOfItems, NumberOfItems*12);
+		for(uint32_t i = 0, s = rawData.size(); i < s; ++i) {
+			dynVec.push_back(rawData[i], ComplexDataSerializer());
+		}
+		
+		CPPUNIT_ASSERT_EQUAL_MESSAGE("size does not match", (uint32_t)rawData.size(), dynVec.size());
+		
+		for(uint32_t i = 0, s = rawData.size(); i < s; ++i) {
+			CPPUNIT_ASSERT_EQUAL_MESSAGE(osmfindlog::toString("item at ", i), rawData[i], dynVec.at(i, ComplexDataDeserializer()));
+		}
+	}
+	
+};
+
+int main() {
+	srand( 0 );
+	CppUnit::TextUi::TestRunner runner;
+	runner.addTest(  DynamicVectorTest<10>::suite() );
+	runner.addTest(  DynamicVectorTest<100>::suite() );
+	runner.addTest(  DynamicVectorTest<1000>::suite() );
+	runner.run();
+	return 0;
+}
