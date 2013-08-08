1419c7ec3ca0c2eb7b44600b7fad611e61bae2d6
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-08-08 16:59:42 +0200

    [tools] mmappedmem tool

---
+++ tools/mmappedmem.cpp
@@ -0,0 +1,11 @@
+#include <sserialize/utility/MmappedMemoy.h>
+#include <iostream>
+
+int main(int argc, char ** argv) {
+	sserialize::UByteArrayAdapter::setTempFilePrefix(argv[0]);
+	uint64_t size = atoll(argv[1]);
+	sserialize::MmappedMemory<int> mem(size);
+	std::cout << "Mapped " << size << " ints" << std::endl;
+	exit(0);
+	return 0;
+}
