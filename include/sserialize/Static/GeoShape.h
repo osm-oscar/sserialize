0473121117972cce753c3c8fd87839d9b02d990e
Daniel Bahrdt <daniel.git@funroll-loops.de>
2013-08-15 13:19:14 +0200

    [Static] GeoSHape header/imp split

--- include/sserialize/Static/GeoShape.h
+++ include/sserialize/Static/GeoShape.h
@@ -1,7 +1,6 @@
 #ifndef SSERIALIZE_STATIC_GEO_SHAPE_H
 #define SSERIALIZE_STATIC_GEO_SHAPE_H
 #include <sserialize/spatial/GeoShape.h>
-#include <sserialize/utility/pack_unpack_functions.h>
 #include <sserialize/Static/GeoPoint.h>
 
 namespace sserialize {
@@ -16,94 +15,30 @@ class GeoShape {
 	UByteArrayAdapter m_data;
 public:
 	GeoShape() : m_type(sserialize::spatial::GS_NONE), m_size(0)  {}
-	GeoShape(const UByteArrayAdapter & data) : m_type(static_cast<sserialize::spatial::GeoShapeType>(data.at(0)) ), m_size(0), m_data(data) {
-		m_data += 1;
-		if (m_type == sserialize::spatial::GS_WAY || m_type == sserialize::spatial::GS_POLYGON) {
-			int len;
-			m_size = m_data.getVlPackedUint32(0, &len);
-			if (len > 0)
-				m_data += len;
-			else
-				m_type = sserialize::spatial::GS_NONE;
-		}
-		else if (m_type == sserialize::spatial::GS_POINT) {
-			m_size = 1;
-		}
-	}
+	GeoShape(const UByteArrayAdapter & data);
 	virtual ~GeoShape() {}
 	
-	uint32_t size() const {
-		return m_size;
-	}
+	inline uint32_t size() const { return m_size; }
 	
-	UByteArrayAdapter::OffsetType getSizeInBytes() const {
-		if (m_type == sserialize::spatial::GS_WAY || sserialize::spatial::GS_POLYGON) {
-			return 1 +  vl_pack_uint32_t_size(m_size) +  SerializationInfo<sserialize::spatial::GeoRect>::length + SerializationInfo<sserialize::Static::spatial::GeoPoint>::length*m_size;
-		}
-		else if (m_data.size() || m_type == sserialize::spatial::GS_POINT) {
-			return 1;
-		}
-		return 0;
-	}
+	UByteArrayAdapter::OffsetType getSizeInBytes() const;
 	
-	sserialize::spatial::GeoShapeType type() const {
-		return m_type;
-	}
+	inline sserialize::spatial::GeoShapeType type() const { return m_type; }
 	
-	GeoPoint at(uint32_t pos) const {
-		if (type() == sserialize::spatial::GS_POINT)
-			return GeoPoint(m_data);
-		else
-			return GeoPoint(m_data + (sserialize::SerializationInfo<sserialize::Static::spatial::GeoPoint>::length*(2+pos)));
-	}
+	GeoPoint at(uint32_t pos) const;
+	sserialize::spatial::GeoRect boundary() const;
 	
-	sserialize::spatial::GeoRect boundary() const {
-		if (m_type == sserialize::spatial::GS_POINT) {
-			GeoPoint p(at(0));
-			return sserialize::spatial::GeoRect(p.latD(), p.latD(), p.lonD(), p.lonD());
-		}
-		else {
-			GeoPoint bL(m_data);
-			GeoPoint tR(m_data+sserialize::SerializationInfo<sserialize::Static::spatial::GeoPoint>::length);
-			return sserialize::spatial::GeoRect(bL.latD(), tR.latD(), bL.lonD(), tR.lonD());
-		}
-	}
+	bool intersects(const sserialize::spatial::GeoRect & boundary) const;
 	
-	bool intersects(const sserialize::spatial::GeoRect & boundary) const {
-		if (type() == sserialize::spatial::GS_POINT) {
-			GeoPoint p(at(0));
-			return boundary.contains(p.latF(), p.lonF());
-		}
-		
-		if (!boundary.overlap( this->boundary()) )
-			return false;
-		uint32_t s = size();
-		UByteArrayAdapter tmp(m_data+2*sserialize::SerializationInfo<sserialize::Static::spatial::GeoPoint>::length); tmp.resetGetPtr();
-		GeoPoint p;
-		for(size_t i = 0; i < s; ++i) {
-			tmp >> p;
-			if (boundary.contains(p.latF(), p.lonF()))
-				return true;
-		}
-		return false;
-	}
-	
-	inline static sserialize::spatial::GeoRect rectFromData(const UByteArrayAdapter &  data) {
-		GeoPoint bL(data);
-		GeoPoint tR(data+sserialize::SerializationInfo<sserialize::Static::spatial::GeoPoint>::length);
-		return sserialize::spatial::GeoRect(bL.latD(), tR.latD(), bL.lonD(), tR.lonD());
-	}
-
+	static sserialize::spatial::GeoRect rectFromData(const UByteArrayAdapter &  data);
 };
 
 }}}//end namespace
 
-inline sserialize::UByteArrayAdapter & operator>>(sserialize::UByteArrayAdapter & in, sserialize::spatial::GeoRect & out) {
-	sserialize::Static::spatial::GeoPoint bL(in);
-	sserialize::Static::spatial::GeoPoint tR(in+sserialize::SerializationInfo<sserialize::Static::spatial::GeoPoint>::length);
-	out = sserialize::spatial::GeoRect(bL.latD(), tR.latD(), bL.lonD(), tR.lonD());
-	in.incGetPtr(2*sserialize::SerializationInfo<sserialize::Static::spatial::GeoPoint>::length);
-	return in;
-}
+sserialize::UByteArrayAdapter & operator>>(sserialize::UByteArrayAdapter & in, sserialize::spatial::GeoRect & out);
 
+namespace sserialize {
+template<>
+sserialize::Static::spatial::GeoShape sserialize::UByteArrayAdapter::get<sserialize::Static::spatial::GeoShape>();
+
+}//end namespace
 #endif
