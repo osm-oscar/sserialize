6a4f7fb1ca56029aae29db6b4cae8568e1b0eb2f
Daniel Bahrdt <daniel.git@funroll-loops.de>
2014-01-15 15:05:51 +0100

    [util] function to check to ranges for subset relation

--- include/sserialize/utility/utilsetfuncs.h
+++ include/sserialize/utility/utilsetfuncs.h
@@ -25,6 +25,24 @@ std::size_t set_intersection_size(TIter1 begin1, TIter1 end1, TIter2 begin2, TIt
 	return size;
 }
 
+///Check if begin1->end1 is a subset of begin2->end2
+template<typename TIter1, typename TIter2>
+bool subset_of(TIter1 begin1, TIter1 end1, TIter2 begin2, TIter2 end2) {
+	while(begin1 != end1 && begin2 != end2) {
+		if (*begin1 < *begin2) {
+			return false;
+		}
+		else if (*begin2 < *begin1) {
+			++begin2;
+		}
+		else {
+			++begin1;
+			++begin2;
+		}
+	}
+	return (begin1 == end1);
+}
+
 template<typename T>
 void diffSortedContainer(std::set<T> & out, const std::set<T> & a, const std::set<T> & b) {
 	std::set<T> result;
