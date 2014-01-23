1f942d142cb056b4cece1457bcf066341a3c8fcd
Daniel Bahrdt <daniel.git@funroll-loops.de>
2014-01-23 16:09:02 +0100

    [util] more hashspecialications

--- include/sserialize/utility/hashspecializations.h
+++ include/sserialize/utility/hashspecializations.h
@@ -1,6 +1,7 @@
 #ifndef SSERIALIZE_HASH_SPECIALIZATIONS_H
 #define SSERIALIZE_HASH_SPECIALIZATIONS_H
 #include <string>
+#include <vector>
 #include <functional>
 
 
@@ -15,6 +16,32 @@ inline void hash_combine(std::size_t & seed, const T & v) {
 inline void hash_combine(uint64_t & seed, const uint8_t v) {
 	seed ^= v + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 }
+
+namespace sserialize {
+
+template<typename TPtr, typename T>
+struct DerefHasher {
+	std::hash<T> hasher;
+	inline size_t operator()(const TPtr & t) const {
+		return hasher(*t);
+	}
+};
+
+template<typename TPtr>
+struct DerefEq {
+	inline bool operator()(const TPtr & t1, const TPtr & t2) const {
+		return *t1 == *t2;
+	}
+};
+
+template<typename TPtr>
+struct DerefSmaler {
+	inline bool operator()(const TPtr & t1, const TPtr & t2) const {
+		return *t1 < *t2;
+	}
+};
+
+}//end namespace sserialize
 
 
 namespace std {
@@ -38,8 +65,17 @@ namespace std {
 			return seed;
 		}
 	};
-	
 
+	template<typename T>
+	struct hash< std::vector<T> > {
+		inline size_t operator()(const std::vector<T> & v) const {
+			size_t seed = 0;
+			for(typename std::vector<T>::const_iterator it(v.begin()), end(v.end()); it != end; ++it) {
+				::hash_combine(seed, *it);
+			}
+			return seed;
+		}
+	};
 
 }
 
