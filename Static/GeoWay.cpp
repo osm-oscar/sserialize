6cc8256f20021d9798da5441adabb569d2fe818b
Daniel Bahrdt <daniel.git@funroll-loops.de>
2014-02-06 16:43:40 +0100

    [Static] implement GeoWay::append

--- Static/GeoWay.cpp
+++ Static/GeoWay.cpp
@@ -6,13 +6,15 @@ namespace spatial {
 namespace detail {
 
 template<>
-UByteArrayAdapter & GeoWay< sserialize::Static::Deque< sserialize::spatial::GeoPoint > >::append(UByteArrayAdapter & destination) const {
-	destination.putVlPackedUint32(points().size());
+UByteArrayAdapter & GeoWay< sserialize::Static::spatial::DenseGeoPointVector >::append(UByteArrayAdapter & destination) const {
 	destination << myBoundary();
-	for(std::size_t i(0), s(points().size()); i < s; ++i) {
-		destination << points().at(i);
-	}
-	return destination;
+	return sserialize::Static::spatial::DenseGeoPointVector::append(cbegin(), cend(), destination);
+// 	destination.putVlPackedUint32(points().size());
+// 	destination << myBoundary();
+// 	for(const_iterator) {
+// 		destination << points().at(i);
+// 	}
+// 	return destination;
 }
 
 }}}//end namespace
