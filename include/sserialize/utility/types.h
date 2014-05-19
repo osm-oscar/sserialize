6c633df37ce159fe33f47856a91ecd48b0b0e815
Daniel Bahrdt <daniel.git@funroll-loops.de>
2014-05-20 00:17:49 +0200

    [util] finer EPS

--- include/sserialize/utility/types.h
+++ include/sserialize/utility/types.h
@@ -24,7 +24,7 @@ namespace sserialize {
 
 #define SSERIALIZED_OFFSET_BYTE_COUNT 5
 #define SSERIALIZED_NEGATIVE_OFFSET_BYTE_COUNT 5
-#define EPSILON 0.0000001
+#define EPSILON 0.00000001d
 
 ///OffsetType >= SizeType >= IdType!
 
