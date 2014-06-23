460b27fafe19521b79ba5f06aec2848f6271c166
Daniel Bahrdt <daniel.git@funroll-loops.de>
2014-06-23 17:50:16 +0200

    [templated] add Option to change HashMap of OADHashTable

--- include/sserialize/templated/OADHashTable.h
+++ include/sserialize/templated/OADHashTable.h
@@ -2,6 +2,7 @@
 #define SSERIALIZE_OAD_HASH_TABLE_H
 #include <vector>
 #include <stdexcept>
+#include <limits>
 
 namespace sserialize {
 namespace detail {
@@ -25,22 +26,40 @@ struct DefaultHash<TValue, false> {
 	inline size_t operator()(const TValue & v) const { return hasher(v); }
 };
 
+template<typename TOADSizeType>
+struct OADSizeTypeLimits {};
+
+template<>
+struct OADSizeTypeLimits<uint32_t> {
+	static constexpr uint32_t max = 0xFFFFFFFF;
+};
+
+template<>
+struct OADSizeTypeLimits<uint64_t> {
+	static constexpr uint64_t max = 0xFFFFFFFFFFFFFFFF;
+};
+
 }}
 
-///An Open addressing double hashed hash table with up to uint32_t::max-1 elements
+///An Open addressing double hashed hash table with up to ValueStorageType::value_type::max-1 elements
 ///element order using the iterators is based on the insertion order
-template<typename TKey, typename TValue, typename THash1 = detail::OADHashTable::DefaultHash<TKey, false>, typename THash2 = detail::OADHashTable::DefaultHash<TKey, true> >
+template<	typename TKey,
+			typename TValue,
+			typename THash1 = detail::OADHashTable::DefaultHash<TKey, false>,
+			typename THash2 = detail::OADHashTable::DefaultHash<TKey, true>,
+			typename TValueStorageType = std::vector< std::pair<TKey, TValue> >,
+			typename TTableStorageType = std::vector<uint32_t> >
 class OADHashTable {
 public:
 	typedef TKey key_type;
 	typedef TValue mapped_type;
-	typedef std::pair<const key_type, mapped_type> value_type;
-	typedef std::vector<value_type> ValueStorageType;
+	typedef std::pair<key_type, mapped_type> value_type;
+	typedef TValueStorageType ValueStorageType;
+	typedef TTableStorageType TableStorageType;
 	typedef typename ValueStorageType::const_iterator const_iterator;
+	typedef typename TableStorageType::value_type SizeType;
 private:
-	typedef std::vector<uint32_t> TableStorageType;
-private:
-	static const uint32_t findend = 0xFFFFFFFF;
+	static constexpr SizeType findend = detail::OADHashTable::OADSizeTypeLimits<SizeType>::max;
 private:
 	ValueStorageType m_valueStorage;
 	TableStorageType m_d;
@@ -49,15 +68,15 @@ private:
 	THash1 m_hash1;
 	THash2 m_hash2;
 private:
-	uint32_t find(const key_type & key) const;
-	void rehash(uint32_t count);
+	SizeType find(const key_type & key) const;
+	void rehash(SizeType count);
 	
 	//pos == 0 is the null-value => real values start with > 0
-	inline value_type & value(uint32_t ptr) {
+	inline value_type & value(SizeType ptr) {
 		return m_valueStorage[ptr-1];
 	}
 	
-	inline const value_type & value(uint32_t ptr) const {
+	inline const value_type & value(SizeType ptr) const {
 		return m_valueStorage[ptr-1];
 	}
 	
@@ -74,41 +93,72 @@ public:
 	m_hash1(hash1),
 	m_hash2(hash2)
 	{}
+	OADHashTable(THash1 hash1, THash2 hash2, double maxLoad, const ValueStorageType & valueStorage, const TableStorageType & tableStorage) :
+	m_valueStorage(valueStorage),
+	m_d(tableStorage),
+	m_maxLoad(maxLoad),
+	m_rehashMult(2.0),
+	m_hash1(hash1),
+	m_hash2(hash2)
+	{
+		m_valueStorage.clear();
+		m_d.clear();
+		m_d.resize(16, 0);
+	}
+	OADHashTable(const ValueStorageType & valueStorage, const TableStorageType & tableStorage) :
+	m_valueStorage(valueStorage),
+	m_d(tableStorage),
+	m_maxLoad(0.8),
+	m_rehashMult(2.0)
+	{
+		m_valueStorage.clear();
+		m_d.clear();
+		m_d.resize(16, 0);
+	}
+	OADHashTable(const OADHashTable & other) :
+	m_valueStorage(other.m_valueStorage),
+	m_d(other.m_d),
+	m_maxLoad(other.m_maxLoad),
+	m_rehashMult(other.m_rehashMult),
+	m_hash1(other.m_hash1),
+	m_hash2(other.m_hash2)
+	{}
 	virtual ~OADHashTable() {}
-	inline uint32_t size() const { return m_valueStorage.size(); }
-	inline uint32_t storageCapacity()  const { return m_valueStorage.capacity();}
+	inline SizeType size() const { return m_valueStorage.size(); }
+	inline SizeType storageCapacity()  const { return m_valueStorage.capacity();}
 	inline double rehashMultiplier() const { return m_rehashMult;}
 	inline void rehashMultiplier(double v) { m_rehashMult = v; }
-	inline uint32_t capacity() const { return m_d.size();}
+	inline SizeType capacity() const { return m_d.size();}
 	inline double load_factor() const { return (double)size()/m_d.size();}
 	inline double max_load_factor() const { return m_maxLoad;}
 	void max_load_factor(double f);
-	void reserve(uint32_t count);
+	void reserve(SizeType count);
 	mapped_type & operator[](const key_type & key);
 	mapped_type & at(const key_type & key);
 	const mapped_type & at(const key_type & key) const;
 	
 	inline bool count(const key_type & key) const {
-		uint32_t pos = find(key);
+		SizeType pos = find(key);
 		return (pos == findend ? false : bool(m_d[pos]));
 	}
 	inline const_iterator cbegin() const { return m_valueStorage.cbegin(); }
 	inline const_iterator cend() const { return m_valueStorage.cend(); }
 };
 
-template<typename TKey, typename TValue, typename THash1, typename THash2>
-uint32_t OADHashTable<TKey, TValue, THash1, THash2>::find(const key_type & key) const {
+template<typename TKey, typename TValue, typename THash1, typename THash2, typename TValueStorageType, typename TTableStorageType>
+typename OADHashTable<TKey, TValue, THash1, THash2, TValueStorageType, TTableStorageType>::SizeType
+OADHashTable<TKey, TValue, THash1, THash2, TValueStorageType, TTableStorageType>::find(const key_type & key) const {
 	uint64_t h1 = (m_hash1(key)) & ~0x1;
-	uint32_t s = m_d.size();
-	uint32_t cpos = h1%s;
-	uint32_t cp = m_d[cpos];
+	SizeType s = m_d.size();
+	SizeType cpos = h1%s;
+	SizeType cp = m_d[cpos];
 	if (!cp || value(cp).first == key) {
 		return cpos;
 	}
 	uint64_t h2 = (m_hash2(key) & ~0x1);
 	for(uint64_t i = 1; i < m_d.size(); ++i) {
 		cpos = (h1 + i*h2) % s;
-		uint32_t cp = m_d[cpos];
+		SizeType cp = m_d[cpos];
 		if (!cp || value(cp).first == key) {
 			return cpos;
 		}
@@ -116,13 +166,14 @@ uint32_t OADHashTable<TKey, TValue, THash1, THash2>::find(const key_type & key) 
 	return findend;
 }
 
-template<typename TKey, typename TValue, typename THash1, typename THash2>
-void OADHashTable<TKey, TValue, THash1, THash2>::rehash(uint32_t count) {
+template<typename TKey, typename TValue, typename THash1, typename THash2, typename TValueStorageType, typename TTableStorageType>
+void
+OADHashTable<TKey, TValue, THash1, THash2, TValueStorageType, TTableStorageType>::rehash(SizeType count) {
 	count = count | 0x1;
-	m_d.resize(0, 0);
+	m_d.clear();
 	m_d.resize(count, 0);
-	for(uint32_t i = 1, s = size(); i <= s; ++i) {
-		uint32_t pos = find(value(i).first);
+	for(SizeType i = 1, s = size(); i <= s; ++i) {
+		SizeType pos = find(value(i).first);
 		if (pos != findend) {
 			m_d[pos] = i;
 		}
@@ -132,11 +183,12 @@ void OADHashTable<TKey, TValue, THash1, THash2>::rehash(uint32_t count) {
 	}
 }
 
-template<typename TKey, typename TValue, typename THash1, typename THash2>
-void OADHashTable<TKey, TValue, THash1, THash2>::max_load_factor(double f) {
+template<typename TKey, typename TValue, typename THash1, typename THash2, typename TValueStorageType, typename TTableStorageType>
+void
+OADHashTable<TKey, TValue, THash1, THash2, TValueStorageType, TTableStorageType>::max_load_factor(double f) {
 	m_maxLoad = f;
 	if (load_factor() > m_maxLoad) {
-		uint32_t targetSize = m_d.size()*m_rehashMult;
+		SizeType targetSize = m_d.size()*m_rehashMult;
 		while ( (double)size()/targetSize > m_maxLoad) {
 			targetSize = targetSize*m_rehashMult;
 		}
@@ -144,8 +196,9 @@ void OADHashTable<TKey, TValue, THash1, THash2>::max_load_factor(double f) {
 	}
 }
 
-template<typename TKey, typename TValue, typename THash1, typename THash2>
-void OADHashTable<TKey, TValue, THash1, THash2>::reserve(uint32_t count) {
+template<typename TKey, typename TValue, typename THash1, typename THash2, typename TValueStorageType, typename TTableStorageType>
+void
+OADHashTable<TKey, TValue, THash1, THash2, TValueStorageType, TTableStorageType>::reserve(SizeType count) {
 	double newTableSize = count/m_maxLoad;
 	if ((double)size()/newTableSize < m_maxLoad) {
 		rehash(newTableSize);
@@ -153,10 +206,11 @@ void OADHashTable<TKey, TValue, THash1, THash2>::reserve(uint32_t count) {
 	m_valueStorage.reserve(count);
 }
 
-template<typename TKey, typename TValue, typename THash1, typename THash2>
-typename OADHashTable<TKey, TValue, THash1, THash2>::mapped_type & OADHashTable<TKey, TValue, THash1, THash2>::operator[](const key_type & key) {
-	uint32_t pos = find(key);
-	uint32_t & cp = m_d[pos]; //saves some calls  m_d[]
+template<typename TKey, typename TValue, typename THash1, typename THash2, typename TValueStorageType, typename TTableStorageType>
+TValue &
+OADHashTable<TKey, TValue, THash1, THash2, TValueStorageType, TTableStorageType>::operator[](const key_type & key) {
+	SizeType pos = find(key);
+	SizeType & cp = m_d[pos]; //saves some calls  m_d[]
 	if (pos != findend && cp) {
 		return value(cp).second;
 	}
@@ -171,20 +225,22 @@ typename OADHashTable<TKey, TValue, THash1, THash2>::mapped_type & OADHashTable<
 	}
 }
 
-template<typename TKey, typename TValue, typename THash1, typename THash2>
-typename OADHashTable<TKey, TValue, THash1, THash2>::mapped_type & OADHashTable<TKey, TValue, THash1, THash2>::at(const key_type & key) {
-	uint32_t pos = find(key);
-	uint32_t & cp = m_d[pos]; //saves some calls  m_d[]
+template<typename TKey, typename TValue, typename THash1, typename THash2, typename TValueStorageType, typename TTableStorageType>
+TValue &
+OADHashTable<TKey, TValue, THash1, THash2, TValueStorageType, TTableStorageType>::at(const key_type & key) {
+	SizeType pos = find(key);
+	SizeType & cp = m_d[pos]; //saves some calls  m_d[]
 	if (pos != findend && cp) {
 		return value(cp).second;
 	}
 	throw std::out_of_range();
 }
 
-template<typename TKey, typename TValue, typename THash1, typename THash2>
-const typename OADHashTable<TKey, TValue, THash1, THash2>::mapped_type & OADHashTable<TKey, TValue, THash1, THash2>::at(const key_type & key) const {
-	uint32_t pos = find(key);
-	uint32_t & cp = m_d[pos]; //saves some calls  m_d[]
+template<typename TKey, typename TValue, typename THash1, typename THash2, typename TValueStorageType, typename TTableStorageType>
+const TValue &
+OADHashTable<TKey, TValue, THash1, THash2, TValueStorageType, TTableStorageType>::at(const key_type & key) const {
+	SizeType pos = find(key);
+	SizeType & cp = m_d[pos]; //saves some calls  m_d[]
 	if (pos != findend && cp) {
 		return value(cp).second;
 	}
