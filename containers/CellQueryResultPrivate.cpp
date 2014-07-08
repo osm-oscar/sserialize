8cb576bf950b64324583571de2132515e0fa0bfd
Daniel Bahrdt <daniel.git@funroll-loops.de>
2014-07-08 10:36:22 +0200

    [containers] improve CellQueryResultPrivate

--- containers/CellQueryResultPrivate.cpp
+++ containers/CellQueryResultPrivate.cpp
@@ -8,7 +8,7 @@ namespace detail {
 CellQueryResult::~CellQueryResult() {
 	std::vector<CellDesc>::const_iterator dIt(m_desc.cbegin()), dEnd(m_desc.cend());
 	IndexDesc * it(m_idx);
-	IndexDesc* end(m_idx+m_desc.size());
+	IndexDesc * end(m_idx+m_desc.size());
 	for(; it != end; ++it, ++dIt) {
 		if (dIt->fetched) {
 			it->idx.sserialize::ItemIndex::~ItemIndex();
@@ -21,7 +21,7 @@ CellQueryResult::~CellQueryResult() {
 void CellQueryResult::uncheckedSet(uint32_t pos, const sserialize::ItemIndex & idx) {
 	new(m_idx+pos) sserialize::ItemIndex(idx);
 }
-	
+
 const sserialize::ItemIndex & CellQueryResult::idx(uint32_t pos) const {
 	const CellDesc & cd = m_desc[pos];
 	if (cd.fetched) {
@@ -34,13 +34,15 @@ const sserialize::ItemIndex & CellQueryResult::idx(uint32_t pos) const {
 	else {
 		idxId = (m_idx+pos)->idxPtr;
 	}
+	//TODO: make this thread-safe
 	const_cast<CellQueryResult*>(this)->uncheckedSet(pos, m_idxStore.at(idxId));
+	const_cast<CellDesc&>(cd).fetched = 1;
 	return (m_idx+pos)->idx;
 }
 
 CellQueryResult * CellQueryResult::intersect(const CellQueryResult * oPtr) const {
 	const CellQueryResult & o = *oPtr;
-	CellQueryResult * rPtr = new CellQueryResult();
+	CellQueryResult * rPtr = new CellQueryResult(m_gh, m_idxStore);
 	CellQueryResult & r = *rPtr;
 	r.m_idx = (IndexDesc*) malloc(sizeof(IndexDesc) * std::min<uint32_t>(m_desc.size(), o.m_desc.size()));
 	
@@ -48,11 +50,13 @@ CellQueryResult * CellQueryResult::intersect(const CellQueryResult * oPtr) const
 	for(uint32_t myI(0), myEnd(m_desc.size()), oI(0), oEnd(o.m_desc.size()); myI < myEnd && oI < oEnd;) {
 		const CellDesc & myCD = m_desc[myI];
 		const CellDesc & oCD = o.m_desc[oI];
-		if (myCD.cellId < oCD.cellId) {
+		uint32_t myCellId = myCD.cellId;
+		uint32_t oCellId = oCD.cellId;
+		if (myCellId < oCellId) {
 			++myI;
 			continue;
 		}
-		else if ( oCD.cellId < myCD.cellId) {
+		else if ( oCellId < myCellId ) {
 			++oI;
 			continue;
 		}
@@ -65,7 +69,7 @@ CellQueryResult * CellQueryResult::intersect(const CellQueryResult * oPtr) const
 				sserialize::ItemIndex res(myPIdx / oPIdx);
 				if (res.size()) {
 					r.uncheckedSet(r.m_desc.size(), res);
-					r.m_desc.push_back(CellDesc(0, 1, myCD.cellId));
+					r.m_desc.push_back(CellDesc(0, 1, myCellId));
 				}
 			}
 			break;
@@ -74,33 +78,35 @@ CellQueryResult * CellQueryResult::intersect(const CellQueryResult * oPtr) const
 				r.uncheckedSet(r.m_desc.size(), m_idx[myI].idx);
 			}
 			else {
-				r.m_idx[m_desc.size()].idxPtr = m_idx[myI].idxPtr;
+				r.m_idx[r.m_desc.size()].idxPtr = m_idx[myI].idxPtr;
 			}
 			r.m_desc.push_back(myCD);
 			break;
 		case 0x2: //my full
 			if (oCD.fetched) {
-				r.uncheckedSet(r.m_desc.size(), o.m_idx[myI].idx);
+				r.uncheckedSet(r.m_desc.size(), o.m_idx[oI].idx);
 			}
 			else {
-				r.m_idx[m_desc.size()].idxPtr = o.m_idx[myI].idxPtr;
+				r.m_idx[r.m_desc.size()].idxPtr = o.m_idx[oI].idxPtr;
 			}
 			r.m_desc.push_back(oCD);
 			break;
 		case 0x3:
 			if (oCD.fetched) {
-				r.uncheckedSet(m_desc.size(), idx(oI));
+				r.uncheckedSet(r.m_desc.size(), o.idx(oI));
 			}
 			else if (myCD.fetched) {
-				r.uncheckedSet(m_desc.size(), idx(myI));
+				r.uncheckedSet(r.m_desc.size(), idx(myI));
 			}
-			r.m_desc.push_back(CellDesc(1, oCD.fetched | myCD.fetched, myCD.cellId));
+			r.m_desc.push_back(CellDesc(1, oCD.fetched | myCD.fetched, myCellId));
 			break;
 		default:
 			break;
 		};
-		r.m_idx = (IndexDesc*) realloc(r.m_idx, r.m_desc.size()*sizeof(IndexDesc));
+		++myI;
+		++oI;
 	}
+	r.m_idx = (IndexDesc*) realloc(r.m_idx, r.m_desc.size()*sizeof(IndexDesc));
 	return rPtr;
 }
 
@@ -116,6 +122,9 @@ CellQueryResult * CellQueryResult::symDiff(const CellQueryResult * other) const 
 	return 0;
 }
 
-CellQueryResult::CellQueryResult() {}
+CellQueryResult::CellQueryResult(const GeoHierarchy & gh, const ItemIndexStore & idxStore) :
+m_gh(gh),
+m_idxStore(idxStore)
+{}
 
 }}//end namespace
--- include/sserialize/containers/CellQueryResultPrivate.h
+++ include/sserialize/containers/CellQueryResultPrivate.h
@@ -13,8 +13,6 @@ public:
 	typedef sserialize::Static::spatial::GeoHierarchy GeoHierarchy;
 	typedef sserialize::Static::ItemIndexStore ItemIndexStore;
 private:
-
-	friend class const_iterator;
 	
 	struct CellDesc {
 		CellDesc(uint32_t inFullMatch, uint32_t inFetched, uint32_t inCellId) {
@@ -22,7 +20,7 @@ private:
 			fetched = inFetched;
 			cellId = inCellId;
 		}
-		uint32_t fullMatch:1;
+		uint32_t fullMatch:1;//about 20% slower but reduces the memory usage by a factor of 5/3
 		uint32_t fetched:1;
 		uint32_t cellId:30;
 	};
@@ -38,17 +36,18 @@ private:
 	IndexDesc * m_idx;
 private:
 	void uncheckedSet(uint32_t pos, const sserialize::ItemIndex & idx);
-	const sserialize::ItemIndex & idx(uint32_t pos) const;
-	
+	CellQueryResult(const GeoHierarchy & gh, const ItemIndexStore & idxStore);
 public:
-	CellQueryResult();
 	///@parameter fmBegin begining of the fully matched cells
-	template<typename T_FM_IT, typename T_PM_IT, typename T_PMITEMSPTR_IT>
-	CellQueryResult(T_FM_IT fmBegin, const T_FM_IT & fmEnd,
-					T_PM_IT pmBegin, const T_PM_IT & pmEnd,
-					T_PMITEMSPTR_IT pmItemsBegin, const T_PMITEMSPTR_IT & pmItemsEnd,
-					const GeoHierarchy & gh, const ItemIndexStore & idxStore);
+	template<typename T_PMITEMSPTR_IT>
+	CellQueryResult(const sserialize::ItemIndex & fmIdx, const sserialize::ItemIndex & pmIdx,
+					T_PMITEMSPTR_IT pmItemsBegin, const GeoHierarchy & gh, const ItemIndexStore & idxStore);
 	virtual ~CellQueryResult();
+
+	const sserialize::ItemIndex & idx(uint32_t pos) const;
+	inline bool fullMatch(uint32_t pos) const { return m_desc[pos].fullMatch; }
+	inline uint32_t cellId(uint32_t pos) const { return m_desc[pos].cellId;}
+	inline  uint32_t cellCount() const { return m_desc.size();}
 	
 	CellQueryResult * intersect(const CellQueryResult * other) const;
 	CellQueryResult * unite(const CellQueryResult * other) const;
@@ -56,16 +55,15 @@ public:
 	CellQueryResult * symDiff(const CellQueryResult * other) const;
 };
 
-template<typename T_FM_IT, typename T_PM_IT, typename T_PMITEMSPTR_IT>
-CellQueryResult::CellQueryResult(T_FM_IT fmIt, const T_FM_IT & fmEnd,
-				T_PM_IT pmIt, const T_PM_IT & pmEnd,
-				T_PMITEMSPTR_IT pmItemsIt, const T_PMITEMSPTR_IT & pmItemsEnd,
-				const GeoHierarchy & gh, const ItemIndexStore & idxStore) :
+template<typename T_PMITEMSPTR_IT>
+CellQueryResult::CellQueryResult(const sserialize::ItemIndex & fmIdx, const sserialize::ItemIndex & pmIdx,
+				T_PMITEMSPTR_IT pmItemsIt, const GeoHierarchy & gh, const ItemIndexStore & idxStore) :
 m_gh(gh),
 m_idxStore(idxStore)
 {
+	sserialize::ItemIndex::const_iterator fmIt(fmIdx.cbegin()), fmEnd(fmIdx.cend()), pmIt(pmIdx.cbegin()), pmEnd(pmIdx.cend());
 
-	uint32_t totalSize = (fmEnd-fmIt) + (pmEnd-pmIt);
+	uint32_t totalSize = fmIdx.size() + pmIdx.size();
 	m_desc.reserve(totalSize);
 	m_idx = (IndexDesc*) malloc(totalSize * sizeof(IndexDesc));
 	IndexDesc * idxPtr = m_idx;
@@ -73,7 +71,7 @@ m_idxStore(idxStore)
 	for(; fmIt != fmEnd && pmIt != pmEnd; ++idxPtr) {
 		uint32_t fCellId = *fmIt;
 		uint32_t pCellId = *pmIt;
-		if(fCellId < pCellId) {
+		if(fCellId <= pCellId) {
 			m_desc.push_back(CellDesc(1, 0, fCellId));
 			++fmIt;
 		}
