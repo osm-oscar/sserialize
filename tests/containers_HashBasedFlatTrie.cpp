2666e5208ba3b1ef4c60c6542259a254a44a882f
Daniel Bahrdt <daniel.git@funroll-loops.de>
2014-08-18 15:35:14 +0200

    [tests] HashbasedFlatTrie

--- tests/containers_HashBasedFlatTrie.cpp
+++ tests/containers_HashBasedFlatTrie.cpp
@@ -6,14 +6,10 @@
 
 const char * inFileName = 0;
 
-inline std::ostream & operator<<(std::ostream & out, const sserialize::detail::HashBasedFlatTrie::StaticString & str) {
-	out.write(str.begin(), str.size());
-	return out;
-}
-
 class TestHashBasedFlatTrie: public CppUnit::TestFixture {
 CPPUNIT_TEST_SUITE( TestHashBasedFlatTrie );
-CPPUNIT_TEST( test );
+CPPUNIT_TEST( testFlatCorrect );
+CPPUNIT_TEST( testNode );
 CPPUNIT_TEST_SUITE_END();
 private:
 	typedef sserialize::HashBasedFlatTrie<uint32_t> MyT;
@@ -47,27 +43,51 @@ public:
 	{
 		std::random_shuffle(m_testStrings.begin(), m_testStrings.end());
 	}
-	virtual void setUp() {}
-	virtual void tearDown() {}
-	void test() {
+	
+	virtual void setUp() {
 		for(const std::string & str : m_testStrings) {
 			m_ht.insert(str);
 		}
 		m_ht.finalize();
-		for(auto x : m_ht) {
-			std::cout << x.first << std::endl;
+	}
+	
+	virtual void tearDown() {}
+	
+	void testFlatCorrect() {
+		CPPUNIT_ASSERT_EQUAL_MESSAGE("size", (uint32_t) m_checkStrings.size(), m_ht.size());
+		MyT::const_iterator tIt(m_ht.cbegin()), tEnd(m_ht.cend());
+		std::vector<std::string>::const_iterator cIt(m_checkStrings.cbegin()), cEnd(m_checkStrings.cend());
+		for(; tIt != tEnd; ++tIt, ++cIt) {
+			CPPUNIT_ASSERT_EQUAL_MESSAGE("node string", *cIt, m_ht.toStr(tIt->first));
 		}
-		{
-			CPPUNIT_ASSERT_EQUAL_MESSAGE("size", (uint32_t) m_checkStrings.size(), m_ht.size());
-			MyT::const_iterator tIt(m_ht.cbegin()), tEnd(m_ht.cend());
-			std::vector<std::string>::const_iterator cIt(m_checkStrings.cbegin()), cEnd(m_checkStrings.cend());
-			for(; tIt != tEnd; ++tIt, ++cIt) {
-				sserialize::detail::HashBasedFlatTrie::StaticString tStr(tIt->first);
-				CPPUNIT_ASSERT_EQUAL_MESSAGE("node string", *cIt, std::string(tStr.begin(), tStr.end()));
+	}
+	
+	//walks the trie in-order and checks for correctness of the Node implementation
+	void testNode() {
+		CPPUNIT_ASSERT_EQUAL_MESSAGE("size", (uint32_t) m_checkStrings.size(), m_ht.size());
+		std::vector< std::pair<MyT::Node::const_iterator, MyT::Node::const_iterator> > nodeIts;
+		uint32_t count = 0;
+		MyT::Node node = m_ht.root();
+		CPPUNIT_ASSERT_EQUAL_MESSAGE("root node", m_checkStrings[count], m_ht.toStr(node.str()));
+		++count;
+		nodeIts.push_back( std::pair<MyT::Node::const_iterator, MyT::Node::const_iterator>(node.begin(), node.end()) );
+		while(nodeIts.size()) {
+			//descend upwards if need be
+			if (nodeIts.back().first == nodeIts.back().second) {
+				while (nodeIts.size() && nodeIts.back().first == nodeIts.back().second) {
+					nodeIts.pop_back();
+				}
+				continue;
 			}
+			MyT::Node::const_iterator & nIt = nodeIts.back().first;
+			node = *nIt;
+			++nIt;
+			//insert children
+			nodeIts.push_back( std::pair<MyT::Node::const_iterator, MyT::Node::const_iterator>(node.begin(), node.end()) );
+			//check the node
+			CPPUNIT_ASSERT_EQUAL_MESSAGE("node string", m_checkStrings[count], m_ht.toStr(node.str()));
+			++count;
 		}
-		sserialize::UByteArrayAdapter tmp(sserialize::UByteArrayAdapter::createCache(1, false));
-		m_ht.append(tmp);
 	}
 };
 
