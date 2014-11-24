379eb722ff63f4cf76cad2856dc3f627a596eee2
Daniel Bahrdt <daniel.git@funroll-loops.de>
2014-11-24 22:59:09 +0100

    [completers] add file layout for StringCompleterPrivateGeoHierrachyUnclustered

--- completers/StringCompleterPrivateGeoHierarchyUnclustered.cpp
+++ completers/StringCompleterPrivateGeoHierarchyUnclustered.cpp
@@ -1,4 +1,7 @@
 #include <sserialize/completers/StringCompleterPrivateGeoHierarchyUnclustered.h>
+#include <sserialize/Static/StringCompleter.h>
+#include <sserialize/utility/exceptions.h>
+#include <sserialize/containers/DynamicBitSet.h>
 
 
 namespace sserialize {
@@ -12,15 +15,34 @@ GeoHierarchyUnclustered::GeoHierarchyUnclustered(const sserialize::Static::spati
 m_gh(gh),
 m_store(store),
 m_ghCompleter(ghCompleter),
-m_directCompleter(itemsCompleter)
+m_itemsCompleter(itemsCompleter)
 {}
+
+GeoHierarchyUnclustered::GeoHierarchyUnclustered(const sserialize::Static::spatial::GeoHierarchy & gh, const sserialize::Static::ItemIndexStore& idxStore, const sserialize::UByteArrayAdapter& data) :
+m_gh(gh),
+m_store(idxStore)
+{
+	SSERIALIZE_VERSION_MISSMATCH_CHECK(SSERIALIZE_GEOHIERARCHY_UNCLUSTERED_COMPLETER_VERSION, data.at(0), "sserialize::Static::StringCompleter::GeoHierarchyUnclustered");
+	sserialize::Static::StringCompleter * ic = new sserialize::Static::StringCompleter(data+1, m_store);
+	sserialize::Static::StringCompleter * gc = 0;
+	try {
+		gc = new sserialize::Static::StringCompleter(data+(1+ic->getSizeInBytes()), m_store);
+	}
+	catch(const std::exception & e) {
+		delete ic;
+		ic = 0;
+		throw e;
+	}
+	m_itemsCompleter.reset(ic);
+	m_ghCompleter.reset(gc);
+}
 
 GeoHierarchyUnclustered::~GeoHierarchyUnclustered() {}
 
 ItemIndex GeoHierarchyUnclustered::complete(const std::string & str, sserialize::StringCompleter::QuerryType qtype) const {
 	ItemIndex ghIdx = m_ghCompleter->complete(str, qtype);
-	ItemIndex itemIdx = m_directCompleter->complete(str, qtype);
-	if (ghIdx.size()) {
+	ItemIndex itemIdx = m_itemsCompleter->complete(str, qtype);
+	if (ghIdx.size() < 1000) {
 		std::vector<ItemIndex> mergeIdcs(ghIdx.size()+1);
 		uint32_t i = 0;
 		for(uint32_t s = ghIdx.size(); i < s; ++i) {
@@ -29,23 +51,33 @@ ItemIndex GeoHierarchyUnclustered::complete(const std::string & str, sserialize:
 		mergeIdcs[i] = itemIdx;
 		return ItemIndex::unite(mergeIdcs);
 	}
+	else if (ghIdx.size() > 1000) {
+		sserialize::DynamicBitSet bitSet;
+		for(uint32_t regionId : ghIdx) {
+			m_store.at(m_gh.regionItemsPtr(ghIdx.at(regionId))).putInto(bitSet);
+		}
+		return sserialize::ItemIndex::fromBitSet(bitSet, m_store.indexType());
+	}
 	return itemIdx;
 }
 
 sserialize::StringCompleter::SupportedQuerries GeoHierarchyUnclustered::getSupportedQuerries() const {
-	return (sserialize::StringCompleter::SupportedQuerries) (m_directCompleter->getSupportedQuerries() & m_ghCompleter->getSupportedQuerries());
+	return (sserialize::StringCompleter::SupportedQuerries) (m_itemsCompleter->getSupportedQuerries() & m_ghCompleter->getSupportedQuerries());
 }
 
 ItemIndex GeoHierarchyUnclustered::indexFromId(uint32_t idxId) const {
-	return m_directCompleter->indexFromId(idxId);
+	return m_itemsCompleter->indexFromId(idxId);
 }
 
 std::ostream& GeoHierarchyUnclustered::printStats(std::ostream& out) const {
 	out << "GeoHierarchyUnclustered::stats--BEGIN" << std::endl;
-	m_directCompleter->printStats(out);
+	out << "ItemsCompleter\n";
+	m_itemsCompleter->printStats(out);
+	out << "GeoHierarchyCompleter\n";
 	m_ghCompleter->printStats(out);
+	out << "GeoHierarchy\n";
 	m_gh.printStats(out);
-	out << "GeoHierarchyUnclustered::stats" << std::endl;
+	out << "GeoHierarchyUnclustered::stats--END" << std::endl;
 	return out;
 }
 	
--- include/sserialize/completers/StringCompleterPrivateGeoHierarchyUnclustered.h
+++ include/sserialize/completers/StringCompleterPrivateGeoHierarchyUnclustered.h
@@ -3,6 +3,16 @@
 #include <sserialize/completers/StringCompleterPrivate.h>
 #include <sserialize/completers/StringCompleter.h>
 #include <sserialize/Static/GeoHierarchy.h>
+#define SSERIALIZE_GEOHIERARCHY_UNCLUSTERED_COMPLETER_VERSION 1
+
+/** Data-Layout
+  *
+  * ----------------------------------------------
+  * VERSION|ItemsCompleter |GeoHierarchyCompleter
+  * ----------------------------------------------
+  *   u8   |StringCompleter|StringCompleter
+  *
+  */
 
 namespace sserialize {
 namespace Static {
@@ -17,10 +27,11 @@ private:
 	sserialize::Static::spatial::GeoHierarchy m_gh;
 	sserialize::Static::ItemIndexStore m_store;
 	sserialize::RCPtrWrapper<sserialize::StringCompleterPrivate> m_ghCompleter;
-	sserialize::RCPtrWrapper<sserialize::StringCompleterPrivate> m_directCompleter;
+	sserialize::RCPtrWrapper<sserialize::StringCompleterPrivate> m_itemsCompleter;
 	
 public:
 	GeoHierarchyUnclustered();
+	GeoHierarchyUnclustered(const sserialize::Static::spatial::GeoHierarchy & gh, const sserialize::Static::ItemIndexStore & idxStore, const sserialize::UByteArrayAdapter & data);
 	GeoHierarchyUnclustered(const MyGeoHierarchy & gh, const sserialize::Static::ItemIndexStore & store, const MyStringCompleter & ghCompleter, const MyStringCompleter & itemsCompleter);
 	virtual ~GeoHierarchyUnclustered();
 	
@@ -33,6 +44,11 @@ public:
 	virtual std::ostream& printStats(std::ostream& out) const;
 	
 	virtual std::string getName() const;
+	
+	inline const sserialize::RCPtrWrapper<sserialize::StringCompleterPrivate> & ghCompleter() const { return m_ghCompleter; }
+	inline sserialize::RCPtrWrapper<sserialize::StringCompleterPrivate> & ghCompleter() { return m_ghCompleter; }
+	inline const sserialize::RCPtrWrapper<sserialize::StringCompleterPrivate> & itemsCompleter() const { return m_itemsCompleter; }
+	inline sserialize::RCPtrWrapper<sserialize::StringCompleterPrivate> & itemsCompleter() { return m_itemsCompleter; }
 	
 };
 
