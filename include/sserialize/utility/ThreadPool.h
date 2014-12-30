896630f64e56a48149cb393382121139e0ac0cb2
Daniel Bahrdt <daniel.git@funroll-loops.de>
2014-12-30 12:27:27 +0100

    [util] improve ThreadPool

--- include/sserialize/utility/ThreadPool.h
+++ include/sserialize/utility/ThreadPool.h
@@ -13,72 +13,44 @@ namespace sserialize {
 class ThreadPool final {
 private:
 	typedef std::function< void(void) > QueuedTaskFunction;
-	class TaskQueue {
-		std::queue< std::function<void(void)> > m_q;
-		bool m_online;
-	public:
-		std::size_t size() const { return m_q.size(); }
-		void pop() { m_q.pop(); }
-		QueuedTaskFunction & front() { return m_q.front(); }
-		void push(QueuedTaskFunction && f) { m_q.push(f); }
-		void push(const QueuedTaskFunction & f) { m_q.push(f); }
-		bool online() const { return m_online; }
-		void online(bool v) { m_online = v; }
-	};
+	typedef std::queue< std::function<void(void)> > TaskQueue;
 private:
 	std::vector<std::thread> m_threads;
 	GuardedVariable<TaskQueue> m_queue;
-	GuardedVariable<uint32_t> m_runningTasks;
+	std::atomic<bool> m_online; //is ThreadPool online
 	GuardedVariable<uint32_t> m_runningThreads;//threads MUST notify this variable if they decrement/increment it
-	std::atomic<bool> m_online;
+	GuardedVariable<uint32_t> m_runningTasks;
 public:
 	void taskWorkerFunc(uint32_t /*myThreadNumber*/) {
-		{
-			auto rtlck(m_runningThreads.uniqueLock());
-			m_runningThreads.value() += 1;
-			m_runningThreads.notify_all(rtlck);
-		}
-		auto lck(m_queue.uniqueLock());
+		m_runningThreads.syncedWithNotifyAll([](uint32_t & v) { v += 1;});
 		while(m_online.load() == true) {
-			m_queue.wait(lck);
-			if (m_online.load() == true && m_queue.value().size()) {
-				QueuedTaskFunction t = m_queue.value().front();
-				m_queue.value().pop();
-				lck.unlock();
-				{
-					auto rtlck(m_runningTasks.uniqueLock());
-					m_runningTasks.value() += 1;
-					m_runningTasks.notify_one();
+			QueuedTaskFunction t;
+			{
+				auto qlck(m_queue.uniqueLock());
+				if (m_queue.unsyncedValue().empty()) {
+					m_queue.wait(qlck);
 				}
-				t();//do the task
-				{
-					auto rtlck(m_runningTasks.uniqueLock());
-					m_runningTasks.value() += 1;
-					m_runningTasks.notify_one();
-				}
+				if (m_queue.unsyncedValue().empty())
+					continue;
+				t = std::move(m_queue.unsyncedValue().front());
+				m_queue.unsyncedValue().pop();
 			}
+			m_runningTasks.syncedWithNotifyAll([](uint32_t & v) { v += 1;});
+			t();//do the task
+			m_runningTasks.syncedWithNotifyAll([](uint32_t & v) { v -= 1;});
 		}
-		{
-			auto rtlck(m_runningThreads.uniqueLock());
-			m_runningThreads.value() -= 1;
-			m_runningThreads.notify_all(rtlck);
-		}
+		m_runningThreads.syncedWithNotifyAll([](uint32_t & v) { v -= 1;});
 	}
 public:
-	ThreadPool(uint32_t numThreads = 1) : m_runningThreads(0), m_online(false) {
+	ThreadPool(uint32_t numThreads = 1) : m_online(false), m_runningThreads(0), m_runningTasks(0) {
 		this->numThreads(numThreads);
-		auto qlck(m_queue.uniqueLock());
-		m_queue.value().online(true);
 	}
-	///processes all the work before destruction (might take some time)
+	///Destroys the thread pool without doing any more work
+	///Call flushQueue() to flush the queue
 	~ThreadPool() {
-		offlineQueue();
-		//wait for empty queue and remaining threads
-		flushQueue();
-		//take the threads offline
 		m_online = false;
 		auto rthlck(m_runningThreads.uniqueLock());
-		while(m_runningThreads.value() > 0) {
+		while(m_runningThreads.unsyncedValue() > 0) {
 			m_queue.notify_one(); //notify a thread in case it's waiting for a new queue element
 			m_runningThreads.wait(rthlck); //is wait atomic for unlock?
 		}
@@ -86,19 +58,11 @@ public:
 			t.join();
 		}
 	}
-	void offlineQueue() {
-		auto qlck(m_queue.uniqueLock());
-		m_queue.value().online(false);
-	}
-	void onlineQueue() {
-		auto qlck(m_queue.uniqueLock());
-		m_queue.value().online(true);
-	}
 	///Wait for empty queue
 	void flushQueue() {
 		auto qlck(m_queue.uniqueLock());
 		auto rtlck(m_runningTasks.uniqueLock());
-		while(m_queue.value().size()) {
+		while(m_queue.unsyncedValue().size() || m_runningTasks.unsyncedValue() > 0) {
 			qlck.unlock();
 			m_runningTasks.wait(rtlck);
 			qlck.lock();
@@ -108,7 +72,7 @@ public:
 		if (num < numThreads()) {
 			m_online.store(false);
 			auto rtlck(m_runningThreads.uniqueLock());
-			while(m_runningThreads.value() > 0) {
+			while(m_runningThreads.unsyncedValue() > 0) {
 				m_runningThreads.wait(rtlck);
 			}
 			for(std::thread & t : m_threads) {
@@ -123,21 +87,13 @@ public:
 		}
 	}
 	uint32_t numThreads() const { return m_threads.size(); }
-	template<typename T_TASKFUNC>
-	bool sheduleTask(T_TASKFUNC t) {
-		auto lck(m_queue.uniqueLock());
-		if (m_queue.value().online()) {
-			m_queue.value().push( QueuedTaskFunction(t) );
-			m_queue.notify_one(lck);
-			return true;
-		}
-		else {
-			return false;
-		}
+	bool sheduleTask(QueuedTaskFunction t) {//std::function als parameter
+		m_queue.syncedWithNotifyOne([&t](TaskQueue & v) { v.push( QueuedTaskFunction(t) );});
+		return true;
 	}
 	template<typename T_TASKFUNC, typename... Args>
-	bool sheduleTaskWithArgs(T_TASKFUNC t, Args...args) {
-		auto tmp = std::bind(t, args...);
+	bool sheduleTaskWithArgs(T_TASKFUNC t, Args&&...args) {
+		auto tmp = std::bind(t, std::forward<Args>(args)...);
 		return sheduleTask(tmp);
 	}
 };
