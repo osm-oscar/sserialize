f5e774a80ff8346116bd7ee47363d4e2e24d1735
Daniel Bahrdt <daniel.git@funroll-loops.de>
2015-01-02 20:23:55 +0100

    [templated] improve GuardedVariable

--- include/sserialize/templated/GuardedVariable.h
+++ include/sserialize/templated/GuardedVariable.h
@@ -11,35 +11,52 @@ public:
 	typedef std::unique_lock<std::mutex> UniqueLock;
 private:
 	T_VALUE m_value;
-	std::mutex m_mtx;
+	mutable std::mutex m_mtx;
 	std::condition_variable m_cv;
 public:
 	GuardedVariable() {}
 	GuardedVariable(const T_VALUE & value) : m_value(value) {}
 	GuardedVariable(const GuardedVariable & other) {
 		UniqueLock mylck(m_mtx);
-		UniqueLock olck(const_cast<GuardedVariable&>(other).m_mtx);//this doesn't look good
+		UniqueLock olck(other.m_mtx);//this doesn't look good
 		m_value = other.m_value;
 	}
 	~GuardedVariable() {}
-	void set(const T_VALUE & value) {
-		      UniqueLock lck(m_mtx);
-		m_value = value;
-	}
 	GuardedVariable & operator=(const T_VALUE & value) {
-		set(value);
+		UniqueLock mylck(m_mtx);
+		m_value = value;
 		return *this;
 	}
 	GuardedVariable & operator=(const GuardedVariable & other) {
 		if (this != &other) {
 			UniqueLock mylck(m_mtx);
-			UniqueLock olck( const_cast<GuardedVariable&>(other).m_mtx);//this doesn't look good
-			m_value = other.value();
+			UniqueLock olck(other.m_mtx);//this doesn't look good
+			m_value = other.unsyncedValue();
 		}
 		return *this;
 	}
-	T_VALUE & value() { return m_value; }
-	const T_VALUE & value() const { return m_value; }
+	template<typename TCallback>
+	void syncedWithoutNotify(TCallback && cb) {
+		UniqueLock lck(m_mtx);
+		cb(m_value);
+	}
+	template<typename TCallback>
+	void syncedWithoutNotify(TCallback && cb) const {
+		UniqueLock lck(m_mtx);
+		cb(m_value);
+	}
+	template<typename TCallback>
+	void syncedWithNotifyOne(TCallback && cb) {
+		syncedWithoutNotify(std::forward<TCallback>(cb));
+		m_cv.notify_one();
+	}
+	template<typename TCallback>
+	void syncedWithNotifyAll(TCallback && cb) {
+		syncedWithoutNotify(std::forward<TCallback>(cb));
+		m_cv.notify_all();
+	}
+	T_VALUE & unsyncedValue() { return m_value; }
+	const T_VALUE & unsyncedValue() const { return m_value; }
 	UniqueLock uniqueLock() { return UniqueLock(m_mtx); }
 	void wait(UniqueLock & ul) {
 		m_cv.wait(ul);
@@ -50,25 +67,19 @@ public:
 	void notify_one() {
 		m_cv.notify_one();
 	}
-	///@param ul unlock lock associated with this variable before notification, don't use this for predictable scheduling behavior
-	void notify_one(UniqueLock & ul) {
-		ul.unlock();
-		m_cv.notify_one();
-	}
 	void notify_all() {
 		m_cv.notify_all();
 	}
-	///@param ul unlock lock before notification, don't use this for predictable scheduling behavior
-	void notify_all(UniqueLock & ul) {
-		ul.unlock();
-		m_cv.notify_all();
-	}
+	//synced cast operator
 	operator T_VALUE() {
 		m_mtx.lock();
 		T_VALUE tmp = m_value;
 		m_mtx.unlock();
 		return tmp;
 	}
+	
+	inline void lock() { m_mtx.lock();}
+	inline void unlock() { m_mtx.unlock(); }
 };
 
 }//end namespace
