dcfb2373aa389f841320b5b3edd6b8820fb7d37b
Daniel Bahrdt <daniel.git@funroll-loops.de>
2015-01-12 00:24:51 +0100

    [templated] add DirectHugheHash::erase and better size() accounting, initialize memory

--- include/sserialize/templated/DirectHugeHash.h
+++ include/sserialize/templated/DirectHugeHash.h
@@ -13,14 +13,16 @@ class DirectHugeHashMap {
 private:
 	uint64_t m_begin;
 	uint64_t m_end;
+	uint64_t m_count;//count only within m_begin and m_end
 	MmappedMemory<TValue> m_data;
 	DynamicBitSet m_bitSet;
 	THashMap m_upperData;
 public:
-	DirectHugeHashMap() : m_begin(0), m_end(0) {}
+	DirectHugeHashMap() : m_begin(0), m_end(0), m_count(0) {}
 	DirectHugeHashMap(uint64_t begin, uint64_t end, MmappedMemoryType mmt) :
 	m_begin(std::min<uint64_t>(begin, end)),
 	m_end(std::max<uint64_t>(begin, end)),
+	m_count(0),
 	m_data(m_end-m_begin, mmt),
 	m_bitSet(UByteArrayAdapter(new std::vector<uint8_t>((m_end-m_begin)/8+1, 0), true))
 	{}
@@ -28,17 +30,19 @@ public:
 	DirectHugeHashMap(uint64_t begin, uint64_t end, MmappedMemoryType mmt, THashParams... params) :
 	m_begin(std::min<uint64_t>(begin, end)),
 	m_end(std::max<uint64_t>(begin, end)),
+	m_count(0),
 	m_data(m_end-m_begin, mmt),
 	m_bitSet(UByteArrayAdapter(new std::vector<uint8_t>((m_end-m_begin)/8+1, 0), true)),
 	m_upperData(params...)
 	{}
 	virtual ~DirectHugeHashMap() {}
+	uint64_t beginDirectRange() const { return m_begin; }
+	uint64_t endDirectRange() const { return m_end; }
 	
 	const THashMap & hashMap() const { return m_upperData; }
 	
-	///This function is lineaer in end-begin!
 	uint64_t size() const {
-		return m_bitSet.size() + m_upperData.size();
+		return m_count + m_upperData.size();
 	}
 	
 	///reserve @count entries in hash map
@@ -46,17 +50,36 @@ public:
 		m_upperData.reserve(count);
 	}
 	
+	///Memory in direct range is not initialized
 	void mark(uint64_t pos) {
 		if (m_begin <= pos && pos < m_end) {
-			m_bitSet.set(pos-m_begin);
+			if (!m_bitSet.isSet(pos-m_begin)) {
+				++m_count;
+				m_bitSet.set(pos-m_begin);
+			}
 		}
 		else {
 			m_upperData[pos] = TValue();
 		}
 	}
 	
-	void insert(uint64_t pos) {
-		mark(pos);
+	///memory in direct range is set to TValue()
+	void erase(uint64_t pos) {
+		if (m_begin <= pos && pos < m_end) {
+			if (m_bitSet.isSet(pos-m_begin)) {
+				--m_count;
+				m_bitSet.unset(pos-m_begin);
+				m_data.data()[pos-m_begin] = TValue();
+			}
+		}
+		else {
+			m_upperData.erase(pos);
+		}
+	}
+	
+	///equal to operator[](pos) = TValue();
+	inline void insert(uint64_t pos) {
+		operator[](pos);
 	}
 	
 	bool count(uint64_t pos) const {
@@ -89,7 +112,11 @@ public:
 	
 	TValue & operator[](uint64_t pos) {
 		if (m_begin <= pos && pos < m_end) {
-			m_bitSet.set(pos-m_begin);
+			if (!m_bitSet.isSet(pos-m_begin)) {
+				++m_count;
+				m_bitSet.set(pos-m_begin);
+				m_data.data()[pos-m_begin] = TValue();
+			}
 			return m_data.data()[pos-m_begin]; 
 		}
 		return m_upperData[pos];
