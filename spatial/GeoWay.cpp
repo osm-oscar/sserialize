74c06d5e7cb1417dd9c6090753c2b4142e08e076
Daniel Bahrdt <daniel.git@funroll-loops.de>
2015-04-21 18:02:21 +0200

    [spatial] GeoWay: remove specialication of append

--- spatial/GeoWay.cpp
+++ spatial/GeoWay.cpp
@@ -1,14 +1,7 @@
 #include <sserialize/spatial/GeoWay.h>
-#include <sserialize/Static/DenseGeoPointVector.h>
 
 namespace sserialize {
 namespace spatial {
 namespace detail {
-
-template<>
-UByteArrayAdapter & GeoWay< std::vector<GeoPoint> >::append(UByteArrayAdapter & destination) const {
-	destination << myBoundary();
-	return sserialize::Static::spatial::DenseGeoPointVector::append(cbegin(), cend(), destination);
-}
 
 }}}//end namespace
