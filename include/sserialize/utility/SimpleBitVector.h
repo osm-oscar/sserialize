88f2293cd450fc2c39582e40e5997f3fe6522415
Daniel Bahrdt <daniel.git@funroll-loops.de>
2015-06-29 15:34:42 +0200

    [util] SimpleBitVector: iterator-based set()

--- include/sserialize/utility/SimpleBitVector.h
+++ include/sserialize/utility/SimpleBitVector.h
@@ -11,6 +11,12 @@ public:
 	inline void set(uint32_t pos) { m_d.at(pos/64) |= (static_cast<uint64_t>(1) << (pos%64)); }
 	inline bool isSet(uint32_t pos) { return m_d.at(pos/64) & (static_cast<uint64_t>(1) << (pos%64)); }
 	inline void reset() { m_d.assign(m_d.size(), 0); }
+	template<typename TIT>
+	void set(TIT begin, const TIT & end) {
+		for(; begin != end; ++begin) {
+			set(*begin);
+		}
+	}
 };
 
 }//end namespace sserialize
