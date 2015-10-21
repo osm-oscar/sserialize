4aaaa30dd766505cd82adaa85a18b6ae8be8056f
Daniel Bahrdt <daniel.git@funroll-loops.de>
2015-10-21 12:39:12 +0200

    [benchmarks] improve oom_sort

--- benchmarks/oom_sort.cpp
+++ benchmarks/oom_sort.cpp
@@ -2,11 +2,11 @@
 #include <sserialize/stats/TimeMeasuerer.h>
 #include <sserialize/utility/printers.h>
 
+void help() {
+	std::cout << "prg -s <size in mebi> -m <memory-size in mebi> -q <queue depth> -t <thread count> -p <temp-path>" << std::endl;
+}
+
 int main(int argc, char ** argv) {
-	if (argc < 2) {
-		std::cout << "prg -s <size in mebi> -m <memory-size in mebi> -q <queue depth> -t <thread count> -p <temp-path>" << std::endl;
-	}
-	
 	uint64_t size = 1 << 30;
 	uint64_t memorySize = 1 << 28;
 	uint32_t threadCount = 2;
@@ -36,7 +36,16 @@ int main(int argc, char ** argv) {
 			sserialize::UByteArrayAdapter::setTempFilePrefix(path);
 			sserialize::UByteArrayAdapter::setFastTempFilePrefix(path);
 		}
+		else if (token == "-h" || token == "--help") {
+			help();
+			return 0;
+		}
 	}
+	if (path.empty()) {
+		help();
+		return -1;
+	}
+	
 	std::cout << "Creating a file at " << path << "\n";
 	std::cout << "File size: " << sserialize::prettyFormatSize(size) << "\n";
 	std::cout << "Memory usage: " << sserialize::prettyFormatSize(memorySize) << std::endl;
@@ -46,10 +55,15 @@ int main(int argc, char ** argv) {
 	sserialize::MMVector<uint64_t> data(sserialize::MM_FILEBASED);
 	data.reserve(entryCount);
 	
+	sserialize::ProgressInfo pinfo;
 	sserialize::TimeMeasurer tm;
-	tm.begin();
+	
+	pinfo.begin(entryCount, "Creating file");
 	for(int64_t i(entryCount); i > 0; --i) {
 		data.push_back(i);
+		if ((i & 0x3FF) == 0) {
+			pinfo(entryCount-i);
+		}
 	}
 	tm.end();
 	std::cout << "File creation took " << tm << std::endl;
