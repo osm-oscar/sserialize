bf66cf54aa54f51980ee395016afdb420a8c04c8
Daniel Bahrdt <daniel.git@funroll-loops.de>
2016-01-22 13:14:54 +0100

    [util] add support for NO_OPTIMIZE on clang

--- include/sserialize/utility/debug.h
+++ include/sserialize/utility/debug.h
@@ -1,7 +1,18 @@
 #ifndef SSERIALIZE_DEBUG_H
 #define SSERIALIZE_DEBUG_H
 
-#define NO_OPTIMIZE __attribute__((optimize(0)))
+#if defined(__clang__)
+	#define NO_OPTIMIZE __attribute__((optimize(0)))
+#elif defined(__GNUC__) || defined(__GNUG__)
+	#define NO_OPTIMIZE __attribute__((optnone))
+#elif defined(_MSC_VER)
+	//what should be here?
+	#define NO_OPTIMIZE __attribute__((optimize(0)))
+#else
+	//bailout?
+	#define NO_OPTIMIZE __attribute__((optimize(0)))
+#endif
+
 #define NO_INLINE __attribute__ ((noinline))
 
 #endif
