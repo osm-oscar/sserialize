337a2a427e32fda316fb2d30275c77b8b23d5d51
Daniel Bahrdt <daniel.git@funroll-loops.de>
2016-01-24 02:39:44 +0100

    [util] optimize was the wrong way round

--- include/sserialize/utility/debug.h
+++ include/sserialize/utility/debug.h
@@ -2,9 +2,9 @@
 #define SSERIALIZE_DEBUG_H
 
 #if defined(__clang__)
-	#define NO_OPTIMIZE __attribute__((optimize(0)))
-#elif defined(__GNUC__) || defined(__GNUG__)
 	#define NO_OPTIMIZE __attribute__((optnone))
+#elif defined(__GNUC__) || defined(__GNUG__)
+	#define NO_OPTIMIZE __attribute__((optimize(0)))
 #elif defined(_MSC_VER)
 	//what should be here?
 	#define NO_OPTIMIZE __attribute__((optimize(0)))
