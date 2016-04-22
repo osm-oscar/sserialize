db5af2c2ba8347e86fd155a872f012d396c8a7ac
Daniel Bahrdt <daniel.git@funroll-loops.de>
2016-04-22 14:41:22 +0200

    [Static] Triangulation::prepare: make multi-round snapping optional (default is 1 round)

--- include/sserialize/Static/Triangulation.h
+++ include/sserialize/Static/Triangulation.h
@@ -341,7 +341,7 @@ public:
 
 	///prepare triangulation for serialization (currently contracts faces that are representable)
 	template<typename T_CTD, typename T_REMOVED_EDGES>
-	static bool prepare(T_CTD& ctd, T_REMOVED_EDGES re);
+	static bool prepare(T_CTD& ctd, T_REMOVED_EDGES re, uint32_t maxRounds = 1);
 	
 	template<typename T_CGAL_TRIANGULATION_DATA_STRUCTURE, typename T_VERTEX_TO_VERTEX_ID_MAP, typename T_FACE_TO_FACE_ID_MAP>
 	static sserialize::UByteArrayAdapter & append(T_CGAL_TRIANGULATION_DATA_STRUCTURE & src, T_FACE_TO_FACE_ID_MAP & faceToFaceId, T_VERTEX_TO_VERTEX_ID_MAP & vertexToVertexId, sserialize::UByteArrayAdapter & dest);
@@ -572,7 +572,7 @@ void Triangulation::explore(uint32_t startFace, T_EXPLORER explorer) const {
 ///You should therefore snap points before creating the triangulation
 ///@return true iff changes were made
 template<typename T_CTD, typename T_REMOVED_EDGES>
-bool Triangulation::prepare(T_CTD & ctd, T_REMOVED_EDGES re = detail::Triangulation::PrintRemovedEdges()) {
+bool Triangulation::prepare(T_CTD & ctd, T_REMOVED_EDGES re = detail::Triangulation::PrintRemovedEdges(), uint32_t maxRounds) {
 	typedef T_CTD TDS;
 	typedef typename TDS::Face_handle Face_handle;
 	typedef typename TDS::Vertex_handle Vertex_handle;
@@ -708,7 +708,7 @@ bool Triangulation::prepare(T_CTD & ctd, T_REMOVED_EDGES re = detail::Triangulat
 	
 	uint32_t totalChangedPoints = 0;
 	bool hadIntersected = true;
-	for(uint32_t round(0), maxRounds(20); hadIntersected && round < maxRounds; ++round) {
+	for(uint32_t round(0); hadIntersected && round < maxRounds; ++round) {
 		bool isLastRound = !(round+1 < maxRounds);
 		hadIntersected = false;
 		
