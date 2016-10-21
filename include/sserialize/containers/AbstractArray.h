0f044d14b38a63900f5556ae67ccddcb37ded7a2
Daniel Bahrdt <daniel.git@funroll-loops.de>
2016-10-21 14:22:42 +0200

    [containers] AbstractArray: Wshorten fixes

--- include/sserialize/containers/AbstractArray.h
+++ include/sserialize/containers/AbstractArray.h
@@ -1,5 +1,6 @@
 #ifndef SSERIALIZE_ABSTRACT_ARRAY_H
 #define SSERIALIZE_ABSTRACT_ARRAY_H
+#include <sserialize/utility/types.h>
 #include <vector>
 #include <memory>
 
@@ -30,11 +31,12 @@ template<typename TReturnType>
 class AbstractArray {
 public:
 	typedef AbstractArrayIterator<TReturnType> * const_iterator;
+	typedef sserialize::SizeType SizeType;
 public:
 	AbstractArray() {}
 	virtual ~AbstractArray() {}
-	virtual uint32_t size() const = 0;
-	virtual TReturnType at(uint32_t pos) const = 0;
+	virtual SizeType size() const = 0;
+	virtual TReturnType at(SizeType pos) const = 0;
 	///function should return a pointer to a AbstractArrayIterator which is then owned by a shared_ptr
 	virtual const_iterator cbegin() const = 0;
 	virtual const_iterator cend() const = 0;
@@ -76,8 +78,8 @@ public:
 	AbstractArrayDefaultImp(const TContainerType & d) : m_data(d) {}
 	virtual ~AbstractArrayDefaultImp() {}
 	const TContainerType & container() const { return m_data; }
-	virtual uint32_t size() const { return m_data.size(); }
-	virtual TReturnType at(uint32_t pos) const { return m_data.at(pos); }
+	virtual SizeType size() const { return m_data.size(); }
+	virtual TReturnType at(SizeType pos) const { return m_data.at(pos); }
 	///function should return a pointer to a AbstractArrayIterator which is then owned by a shared_ptr
 	virtual typename MyBaseClass::const_iterator cbegin() const { return new const_iterator(m_data.cbegin());}
 	virtual typename MyBaseClass::const_iterator cend() const { return new const_iterator(m_data.cend());}
@@ -141,6 +143,7 @@ public:
 	typedef TReference reference;
 	typedef TConstReference const_reference;
 	typedef std::shared_ptr< detail::AbstractArray<TReturnType> > ImpRefType;
+	typedef typename detail::AbstractArray<TReturnType>::SizeType SizeType;
 private:
 	ImpRefType m_priv;
 public:
@@ -151,8 +154,8 @@ public:
 	const ImpRefType & priv() const { return m_priv; }
 	template<typename TAbstractArrayType>
 	TAbstractArrayType * get() const { return static_cast<TAbstractArrayType*>(m_priv.get()); }
-	uint32_t size() const { return m_priv->size();}
-	const_reference at(uint32_t pos) const { return m_priv->at(pos); }
+	SizeType size() const { return m_priv->size();}
+	const_reference at(SizeType pos) const { return m_priv->at(pos); }
 	const_reference front() const { return m_priv->at(0); }
 	const_reference back() const { return m_priv->at(size()-1);}
 	const_iterator cbegin() const { return const_iterator(m_priv->cbegin()); }
