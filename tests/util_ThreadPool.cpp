b9b8a4fad5f9b65bc6d9d5b499509f1958714dfb
Daniel Bahrdt <daniel.git@funroll-loops.de>
2016-11-18 23:14:58 +0100

    [tests] improve ThreadPool test

--- tests/util_ThreadPool.cpp
+++ tests/util_ThreadPool.cpp
@@ -1,6 +1,7 @@
 #include <sserialize/mt/ThreadPool.h>
 #include <sserialize/mt/GuardedVariable.h>
 #include "TestBase.h"
+#include <unistd.h>
 
 template<uint32_t T_NUM_THREADS, uint32_t T_NUM_TASKS>
 class TestThreadPool: public sserialize::tests::TestBase {
@@ -13,13 +14,31 @@ public:
 	void test() {
 		sserialize::ThreadPool tp(T_NUM_THREADS);
 		sserialize::GuardedVariable<uint32_t> gv(0);
+		std::atomic<uint32_t> scheduledTasks(0);
+		std::atomic<uint32_t> fcalls(0);
+		std::atomic<uint32_t> gvcalls(0);
+		CPPUNIT_ASSERT_EQUAL((uint32_t)0, gv.unsyncedValue());
 		for(uint32_t i = 0; i < T_NUM_TASKS; ++i) {
-			tp.sheduleTask([&gv](){
-				gv.syncedWithoutNotify([](uint32_t & v) { v += 1; });
+			bool ok = tp.sheduleTask([&gv, &fcalls, &gvcalls](){
+				fcalls += 1;
+				gv.syncedWithoutNotify([&gvcalls](uint32_t & v) {
+					v += 1;
+					gvcalls += 1;
+				});
 			});
+			if (ok) {
+				scheduledTasks += 1;
+			}
 		}
 		tp.flushQueue();
-		CPPUNIT_ASSERT_EQUAL_MESSAGE("number of tasks completed", T_NUM_TASKS, gv.unsyncedValue());
+		std::size_t qs = tp.queueSize();
+		uint32_t v;
+		gv.syncedWithoutNotify([&v](uint32_t iv) { v = iv; });
+		CPPUNIT_ASSERT_EQUAL_MESSAGE("scheduled tasks", T_NUM_TASKS, scheduledTasks.load());
+		CPPUNIT_ASSERT_EQUAL_MESSAGE("fcalls", T_NUM_TASKS, fcalls.load());
+		CPPUNIT_ASSERT_EQUAL_MESSAGE("gvcalls", T_NUM_TASKS, gvcalls.load());
+		CPPUNIT_ASSERT_EQUAL_MESSAGE("number of tasks in queue", (std::size_t) 0, qs);
+		CPPUNIT_ASSERT_EQUAL_MESSAGE("number of tasks completed", (uint32_t) T_NUM_TASKS, v);
 	}
 };
 
