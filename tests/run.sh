0edf0943ac83b99bcf248ac4b99e6e430deb3451
Daniel Bahrdt <daniel.git@funroll-loops.de>
2016-11-25 13:37:04 +0100

    improve run.sh to select the tests that should be run

--- tests/run.sh
+++ tests/run.sh
@@ -9,6 +9,7 @@ SLOW_TESTS=("sserializetests_algorithm_oom_sort"
 	"sserializetests_util_compactuintarray"
 	"sserializetests_util_CompressedMmappedFile"
 	"sserializetests_util_MmappedMemory"
+	"sserializetests_static_Triangulation"
 	)
 
 NORMAL_TESTS=(
@@ -16,7 +17,6 @@ NORMAL_TESTS=(
 	"sserializetests_containers_multivarbitarray"
 	"sserializetests_containers_VariantStore"
 	"sserializetests_static_deque"
-	"sserializetests_static_Triangulation"
 	"sserializetests_templated_huffmancode"
 )
 
@@ -76,6 +76,13 @@ SHM_FILE_PREFIX="sserializeteststmp"
 
 BUILD_PATH="${1}"
 
+TEST_SELECTION="${2}"
+
+if [ -z "${TEST_SELECTION}" ]; then
+	echo "No test selected. Select with run.sh build-path SELECTION where selection is a combination of snf";
+	exit 1
+fi
+
 if [ ! -d "${BUILD_PATH}" ] || [ ! -f "${BUILD_PATH}/sserializetests_util_UByteArrayAdapter" ]; then
 	echo "Invalid build path given"
 	exit 1
@@ -108,16 +115,51 @@ echo "#Slow tests: $num_slow"
 echo "#Normal tests: $num_normal"
 echo "#Fast tests: $num_fast"
 
-for ((i = 0; i < ${#FAST_TESTS[@]}; ++i)); do
-	echo "Executing $i: ${FAST_TESTS[i]}"
-	eval "./${FAST_TESTS[i]} ${TMP_FILE_OPTS}"
-	FAST_TESTS_RESULT[i]=$?
-	ls -1 /tmp/sserialize*
-	if [ $? -eq 0 ]; then
-		echo "tmp broken for $i"
-		exit 1
-	fi
-done
+echo "${TEST_SELECTION}" | egrep "f"
+if [ $? -eq 0 ]; then
+	for ((i = 0; i < ${#FAST_TESTS[@]}; ++i)); do
+		echo "Executing $i: ${FAST_TESTS[i]}"
+		eval "./${FAST_TESTS[i]} ${TMP_FILE_OPTS}"
+		FAST_TESTS_RESULT[i]=$?
+		ls -1 /tmp/sserialize*
+		if [ $? -eq 0 ]; then
+			echo "tmp broken for $i"
+			exit 1
+		fi
+	done
+fi
+
+echo "${TEST_SELECTION}" | egrep "n"
+if [ $? -eq 0 ]; then
+	#now the NORMAL TESTS
+	for ((i = 0; i < ${#NORMAL_TESTS[@]}; ++i)); do
+		echo "Executing $i: ${NORMAL_TESTS[i]}"
+		eval "./${NORMAL_TESTS[i]} ${TMP_FILE_OPTS}"
+		NORMAL_TESTS_RESULT[i]=$?
+		ls -1 /tmp/sserialize*
+		if [ $? -eq 0 ]; then
+			echo "tmp broken for $i"
+			exit 1
+		fi
+	done
+fi
+
+echo "${TEST_SELECTION}" | egrep "s"
+if [ $? -eq 0 ]; then
+	#slow tests
+	for ((i = 0; i < ${#SLOW_TESTS[@]}; ++i)); do
+		echo "Executing $i: ${SLOW_TESTS[i]}"
+		eval "./${SLOW_TESTS[i]} ${TMP_FILE_OPTS}"
+		SLOW_TESTS_RESULT[i]=$?
+		ls -1 /tmp/sserialize*
+		if [ $? -eq 0 ]; then
+			echo "tmp broken for $i"
+			exit 1
+		fi
+	done
+fi
+
+#and now the result output
 
 for ((i = 0; i < ${#FAST_TESTS_RESULT[@]}; ++i)); do
 	if [ ${FAST_TESTS_RESULT[i]} -ne 0 ]; then
@@ -126,25 +168,20 @@ for ((i = 0; i < ${#FAST_TESTS_RESULT[@]}; ++i)); do
 	fi
 done
 
+for ((i = 0; i < ${#NORMAL_TESTS_RESULT[@]}; ++i)); do
+	if [ ${NORMAL_TESTS_RESULT[i]} -ne 0 ]; then
+		echo "Test number $i = ${NORMAL_TESTS[i]} failed"
+		TEST_FAILED=1
+	fi
+done
+
+for ((i = 0; i < ${#SLOW_TESTS_RESULT[@]}; ++i)); do
+	if [ ${SLOW_TESTS_RESULT[i]} -ne 0 ]; then
+		echo "Test number $i = ${SLOW_TESTS[i]} failed"
+		TEST_FAILED=1
+	fi
+done
+
 if [ ${TEST_FAILED} -eq 1 ]; then
 	exit 1
 fi
-
-# for ((i = 0; i < ${#NORMAL_TESTS[@]}; ++i)); do
-# 	echo "Executing $i: ${NORMAL_TESTS[i]}"
-# 	eval "./${NORMAL_TESTS[i]}"
-# 	RET=$?
-# 	if [ $RET -ne 0 ]; then
-# 		exit 1
-# 	fi
-# done
-# 
-# for ((i = 0; i < ${#SLOW_TESTS[@]}; ++i)); do
-# 	echo "Executing $i:"
-# 	eval "./${SLOW_TESTS[i]}"
-# 	RET=$?
-# 	if [ $RET -ne 0 ]; then
-# 		exit 1
-# 	fi
-# done
-
