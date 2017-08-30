3adc3e9c78ec917a42eb8e57f131715de72d6d64
Daniel Bahrdt <daniel.git@funroll-loops.de>
2017-08-30 16:55:01 +0200

    [iterator] untested UnaryCodeIterator

---
+++ include/sserialize/iterator/UnaryCodeIterator.h
@@ -0,0 +1,65 @@
+#ifndef SSERIALIZE_UNARY_CODE_ITERATOR_H
+#define SSERIALIZE_UNARY_CODE_ITERATOR_H
+#include <sserialize/storage/UByteArrayAdapter.h>
+#include <sserialize/storage/SerializationInfo.h>
+
+namespace sserialize {
+
+/** Simple unary code:
+  * A number n is encoded as n zeros followed by a 1:
+  * 4 -> 00001
+  *
+  * A stream is split into chunks with the least-significant bit to the right
+  *
+  */
+  
+class UnaryCodeCreator;
+
+class UnaryCodeIterator: std::iterator<std::input_iterator_tag, uint32_t> {
+public:
+	UnaryCodeIterator();
+	UnaryCodeIterator(const sserialize::UByteArrayAdapter & d);
+	UnaryCodeIterator(const UnaryCodeIterator & other) = default;
+	~UnaryCodeIterator();
+public:
+	value_type operator*() const;
+	UnaryCodeIterator & operator++();
+public:
+	bool operator=(UnaryCodeIterator const & other) const;
+	bool operator!=(UnaryCodeIterator const & other) const;
+private:
+	friend class UnaryCodeCreator;
+private:
+	using chunk_type = uint8_t;
+	static constexpr uint32_t chunk_bits = std::numeric_limits<chunk_type>::digits;
+	static constexpr uint32_t chunk_size = SerializationInfo<chunk_type>::length;
+	static constexpr chunk_type chunk_max_bit = chunk_type(1) << (chunk_bits-1);
+private:
+	void loadNextChunk();
+private:
+	UByteArrayAdapter m_d;
+	UByteArrayAdapter::SizeType m_pos; //always points to the next entry
+	value_type m_last;
+	chunk_type m_lastChunk;
+	chunk_type m_chunkBitPtr;
+};
+
+class UnaryCodeCreator {
+public:
+	UnaryCodeCreator(UByteArrayAdapter & d);
+	~UnaryCodeCreator();
+	void put(UnaryCodeIterator::value_type v);
+	void flush();
+private:
+	using chunk_type = UnaryCodeIterator::chunk_type;
+	static constexpr uint32_t chunk_bits = std::numeric_limits<chunk_type>::digits;
+	static constexpr uint32_t chunk_size = SerializationInfo<chunk_type>::length;
+private:
+	UByteArrayAdapter & m_d;
+	chunk_type m_lastChunk;
+	int8_t m_inChunkBits; //number of remaining bits in chunk
+};
+
+} //end namespace
+
+#endif
---
+++ src/iterator/UnaryCodeIterator.cpp
@@ -0,0 +1,133 @@
+#include <sserialize/iterator/UnaryCodeIterator.h>
+
+namespace sserialize {
+
+UnaryCodeIterator::UnaryCodeIterator() :
+m_d(),
+m_pos(0),
+m_last(0),
+m_lastChunk(0),
+m_chunkBitPtr(0)
+{}
+
+UnaryCodeIterator::UnaryCodeIterator(const UByteArrayAdapter& d) :
+m_d(d),
+m_pos(0),
+m_last(0),
+m_lastChunk(0),
+m_chunkBitPtr(chunk_max_bit)
+{
+	operator++();
+}
+
+UnaryCodeIterator::~UnaryCodeIterator() {}
+
+UnaryCodeIterator::value_type
+UnaryCodeIterator::operator*() const {
+	return m_last;
+}
+
+UnaryCodeIterator&
+UnaryCodeIterator::operator++() {
+	if (m_pos >= m_d.size() && !m_chunkBitPtr) {
+		return *this;
+	}
+	m_last = 0;
+	
+	if (!m_chunkBitPtr) {
+		loadNextChunk();
+	}
+	
+	if (m_chunkBitPtr != chunk_max_bit) { //there are still bits in our current byte
+		while (m_chunkBitPtr) {
+			bool isStopBit = (m_lastChunk & m_chunkBitPtr);
+			m_chunkBitPtr >>= 1;
+			
+			if (isStopBit) {
+				return *this;
+			}
+			else {
+				m_last += 1;
+			}
+		}
+	}
+	//from here on we only have full bytes
+	m_chunkBitPtr = chunk_max_bit;
+	while (m_pos < m_d.size()) {
+		loadNextChunk();
+		if (m_lastChunk) { //our stop bit is inside
+			break;
+		}
+		else {
+			m_last += chunk_bits;
+		}
+	}
+	
+	//we still have to find our stop bit
+	while (m_chunkBitPtr) {
+		bool isStopBit = (m_lastChunk & m_chunkBitPtr);
+		m_chunkBitPtr >>= 1;
+		
+		if (isStopBit) {
+			break;
+		}
+		else {
+			m_last += 1;
+		}
+	}
+	
+	return *this;
+}
+
+bool UnaryCodeIterator::operator!=(const UnaryCodeIterator& other) const {
+	return m_pos != other.m_pos || m_chunkBitPtr != other.m_chunkBitPtr || m_last != other.m_last || m_d != other.m_d;
+}
+
+bool UnaryCodeIterator::operator=(const UnaryCodeIterator& other) const {
+	return m_pos == other.m_pos && m_chunkBitPtr == other.m_chunkBitPtr && m_last == other.m_last && m_d == other.m_d;
+}
+
+void UnaryCodeIterator::loadNextChunk() {
+	m_lastChunk = m_d.get<chunk_type>(m_pos);
+	m_chunkBitPtr = chunk_max_bit;
+	m_pos += SerializationInfo<chunk_type>::length;
+}
+
+// creator
+
+UnaryCodeCreator::UnaryCodeCreator(UByteArrayAdapter& d) :
+m_d(d),
+m_lastChunk(0),
+m_inChunkBits(chunk_bits)
+{}
+
+UnaryCodeCreator::~UnaryCodeCreator() {
+	flush();
+}
+
+void UnaryCodeCreator::put(uint32_t v) {
+	if (v >= uint32_t(m_inChunkBits) ) {
+		m_d.put(m_lastChunk);
+		v -= m_inChunkBits;
+		
+		m_inChunkBits = chunk_bits;
+		m_lastChunk = 0;
+		
+		for(;v >= chunk_bits; v -= chunk_bits) {
+			m_d.put(m_lastChunk);
+		}
+	}
+	
+	SSERIALIZE_CHEAP_ASSERT_SMALLER(v, uint32_t(m_inChunkBits));
+	
+	m_lastChunk |= chunk_type(1) << (m_inChunkBits-(v+1));
+	m_inChunkBits -= (v+1);
+}
+
+void UnaryCodeCreator::flush() {
+	if (m_lastChunk) {
+		m_d.put(m_lastChunk);
+	}
+}
+
+}//end namespace sserialize
