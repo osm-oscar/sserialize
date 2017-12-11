44a0a548571a8224dd71e37e9cfb35fee0e26a85
Daniel Bahrdt <daniel.git@funroll-loops.de>
2017-12-11 13:07:44 +0000

    [bench] more improvements

--- benchmarks/treevector-stdset-merge-comparisson.cpp
+++ benchmarks/treevector-stdset-merge-comparisson.cpp
@@ -3,32 +3,67 @@
 #include <set>
 #include <queue>
 #include <sserialize/containers/ItemIndexFactory.h>
+#include <random>
 
-std::vector<uint32_t> createNumbersSet(uint32_t count) {
+
+std::default_random_engine random_engine(0);
+
+std::vector<uint32_t> createNumbersSet(uint32_t count, uint32_t max) {
 	std::set<uint32_t> ret;
 	//Fill the first
-	uint32_t rndNum;
-	uint32_t rndMask;
-	uint32_t mask;
-	while( ret.size() < count) {
-		rndNum = rand();
-		rndMask = (double)rand()/RAND_MAX * 31; 
-		mask = ((rndMask+1 == 32) ? 0xFFFFFFFF : ((static_cast<uint32_t>(1) << (rndMask+1)) - 1));
-		uint32_t key = rndNum & mask;
-		ret.insert(key);
+	std::uniform_int_distribution<int> d(1, max);
+	
+	for(uint32_t i(0); i < count; ++i) {
+		ret.insert( d(random_engine) );
 	}
 	return std::vector<uint32_t>(ret.begin(), ret.end());
+}
+
+static std::vector<uint32_t> merge(const std::vector<uint32_t> & a, const std::vector<uint32_t> & b) {
+	std::vector<uint32_t> result;
+	result.resize(a.size()+b.size());
+	auto aIt(a.cbegin());
+	auto aEnd(a.cend());
+	auto bIt(b.cbegin());
+	auto bEnd(b.cend());
+	auto rIt(result.begin());
+	while (aIt != aEnd && bIt != bEnd) {
+		if (*aIt == *bIt) {
+			*rIt = *aIt;
+			++aIt;
+			++bIt;
+		}
+		else if (*aIt < *bIt) {
+			*rIt = *aIt;
+			++aIt;
+		}
+		else { //bItemId is smaller
+			*rIt = *bIt;
+			++bIt;
+		}
+		++rIt;
+	}
+	for(; aIt != aEnd; ++aIt, ++rIt) {
+		*rIt = *aIt;
+	}
+	for(; bIt != bEnd; ++bIt, ++rIt) {
+		*rIt = *bIt;
+	}
+	result.resize(rIt-result.begin());
+	return result;
 }
 
 enum IndexType {
 	__IT_FIRST_OWN=2*sserialize::ItemIndex::__T_LAST_ENTRY,
 	IT_VECTOR_TREE_MERGE=__IT_FIRST_OWN,
 	IT_VECTOR_SET_MERGE=2*IT_VECTOR_TREE_MERGE,
-	IT_VECTOR_HEAP_MERGE=2*IT_VECTOR_SET_MERGE
+	IT_VECTOR_HEAP_MERGE=2*IT_VECTOR_SET_MERGE,
+	__IT_MERGE_WITH_VECTOR=2*IT_VECTOR_HEAP_MERGE,
+	__IT_MERGE_WITH_HEAP=2*__IT_MERGE_WITH_VECTOR
 };
 
 struct TestDataBase {
-	using meas_res = std::chrono::microseconds;
+	using meas_res = std::chrono::milliseconds;
 	std::vector<meas_res> times;
 	
 	 virtual ~TestDataBase() {}
@@ -60,13 +95,103 @@ struct ItemIndexTestData: TestDataBase {
 	}
 	virtual ~ItemIndexTestData() override {}
 	virtual void run_merge() override {
-		sserialize::ItemIndex idx = sserialize::ItemIndex::unite(buckets);
+		sserialize::ItemIndex result = sserialize::ItemIndex::unite(buckets);
+		std::cout << name() << "::result-size: " << result.size() << std::endl;
 	}
 	virtual std::string name() const {
 		return "ItemIndex::" + sserialize::to_string(t);
 	}
 	virtual int type() const {
 		return t;
+	}
+};
+
+struct ItemIndexMergeWithVectorTestData: ItemIndexTestData {
+	ItemIndexMergeWithVectorTestData(const std::vector<std::vector<uint32_t>> & src, sserialize::ItemIndex::Types t) :
+	ItemIndexTestData(src, t)
+	{}
+	virtual ~ItemIndexMergeWithVectorTestData() override {}
+	virtual void run_merge() override {
+		std::vector<uint32_t> result = sserialize::treeReduceMap<std::vector<sserialize::ItemIndex>::const_iterator, std::vector<uint32_t> >(
+			buckets.cbegin(), buckets.cend(),
+			[](const std::vector<uint32_t> & a, const std::vector<uint32_t> & b) {
+				return merge(a, b);
+			},
+			[](const sserialize::ItemIndex & a) {
+				return a.toVector();
+			}
+		);
+		std::cout << name() << "::result-size: " << result.size() << std::endl;
+	}
+	virtual std::string name() const {
+		return "ItemIndex-with-vector::" + sserialize::to_string(t);
+	}
+	virtual int type() const {
+		return ItemIndexTestData::type() | __IT_MERGE_WITH_VECTOR;
+	}
+};
+
+struct ItemIndexHeapMergeTestData: ItemIndexTestData {
+	struct HeapElement {
+		using iterator = sserialize::ItemIndex::const_iterator;
+		iterator it;
+		iterator end;
+		HeapElement(const iterator & it, const iterator & end) : it(it), end(end) {}
+		bool operator<(const HeapElement & other) const {
+			return *it > *other.it; //prio-queue is a max heap, but we need a min-heap!
+		}
+		uint32_t value() const { return *it; }
+		bool next() {
+			if (it != end) {
+				++it;
+				return it != end;
+			}
+			return false;
+		}
+	};
+
+	ItemIndexHeapMergeTestData(const std::vector<std::vector<uint32_t>> & src, sserialize::ItemIndex::Types t) :
+	ItemIndexTestData(src, t)
+	{}
+	virtual ~ItemIndexHeapMergeTestData() override {}
+	virtual void run_merge() override {
+		std::vector<uint32_t> result;
+		std::priority_queue<HeapElement> queue;
+		for(const auto & x : buckets) {
+			if (x.size()) {
+				queue.emplace(x.cbegin(), x.cend());
+			}
+		}
+		if (queue.empty()){
+			return;
+		}
+		
+		//extract first element, this way we don't have to check whether result is empty
+		{
+			HeapElement top = queue.top();
+			queue.pop();
+			result.emplace_back( top.value() );
+			if (top.next()) {
+				queue.push(top);
+			}
+		}
+		while(!queue.empty()) {
+			HeapElement top = queue.top();
+			queue.pop();
+			if (result.back() != top.value()) {
+				result.emplace_back( top.value() );
+			}
+			if (top.next()) {
+				queue.push(top);
+			}
+		}
+		std::cout << name() << "::result-size: " << result.size() << std::endl;
+	}
+	virtual std::string name() const {
+		return "ItemIndex-with-heap::" + sserialize::to_string(t);
+	}
+	virtual int type() const {
+		return ItemIndexTestData::type() | __IT_MERGE_WITH_HEAP;
 	}
 };
 
@@ -77,19 +202,12 @@ struct VectorTreeMergeTestData: TestDataBase {
 	
 	virtual void run_merge() override {
 		auto result = sserialize::treeReduce(
-			buckets->begin(), buckets->end(),
+			buckets->cbegin(), buckets->cend(),
 			[](const std::vector<uint32_t> & a, const std::vector<uint32_t> & b) {
-				std::vector<uint32_t> result;
-				result.reserve( a.size() + b.size() );
-				
-				std::merge(
-					a.begin(), a.end(),
-					b.begin(), b.end(),
-					std::back_insert_iterator<decltype(result)>(result)
-				);
-				return result;
+				return merge(a, b);
 			}
 		);
+		std::cout << name() << "::result-size: " << result.size() << std::endl;
 	}
 	virtual std::string name() const {
 		return "std::vector::tree-merge";
@@ -108,6 +226,7 @@ struct VectorSetMergeTestData: TestDataBase {
 		for(const auto & x : *buckets) {
 			result.insert(x.cbegin(), x.cend());
 		}
+		std::cout << name() << "::result-size: " << result.size() << std::endl;
 	}
 	virtual std::string name() const {
 		return "std::vector::set-merge";
@@ -127,11 +246,8 @@ struct VectorHeapMergeTestData: TestDataBase {
 		}
 		uint32_t value() const { return *it; }
 		bool next() {
-			if (it != end) {
-				++it;
-				return true;
-			}
-			return false;
+			++it;
+			return it < end;
 		}
 	};
 
@@ -170,6 +286,7 @@ struct VectorHeapMergeTestData: TestDataBase {
 				queue.push(top);
 			}
 		}
+		std::cout << name() << "::result-size: " << result.size() << std::endl;
 	}
 	virtual std::string name() const {
 		return "std::vector::heap-merge";
@@ -188,7 +305,7 @@ struct TestData {
 	void init(uint32_t num_buckets, uint32_t bucket_fill) {
 		source.reserve(num_buckets);
 		for(uint32_t i(0); i < num_buckets; ++i) {
-			source.emplace_back( createNumbersSet(bucket_fill) );
+			source.emplace_back( createNumbersSet(bucket_fill, sserialize::narrow_check<uint32_t>(num_buckets*bucket_fill)) );
 		}
 		
 		auto mergef = [this](const std::vector<uint32_t> & a, const std::vector<uint32_t> & b) {
@@ -219,7 +336,8 @@ struct TestData {
 			result.insert(result.end(), bIt, bEnd);
 			return result;
 		};
-		result = sserialize::treeReduce(source.begin(), source.end(), mergef);
+		result = sserialize::treeReduce(source.cbegin(), source.cend(), mergef);
+		std::cout << "initial result size: " << result.size() << std::endl;
 	}
 	
 	void add_test(int t) {
@@ -228,6 +346,12 @@ struct TestData {
 		}
 		if (t < __IT_FIRST_OWN) {
 			data[t].reset(new ItemIndexTestData(source, (sserialize::ItemIndex::Types)t) );
+		}
+		else if (__IT_MERGE_WITH_VECTOR & t) {
+			data[t].reset(new ItemIndexMergeWithVectorTestData(source, (sserialize::ItemIndex::Types)(t & ~(__IT_MERGE_WITH_VECTOR))));
+		}
+		else if (__IT_MERGE_WITH_HEAP & t) {
+			data[t].reset(new ItemIndexHeapMergeTestData(source, (sserialize::ItemIndex::Types)(t & ~(__IT_MERGE_WITH_HEAP))));
 		}
 		else {
 			switch (t) {
@@ -298,7 +422,13 @@ int main(int argc, char ** argv) {
 		sserialize::ItemIndex::T_NATIVE,
 		sserialize::ItemIndex::T_WAH,
 // 		sserialize::ItemIndex::T_ELIAS_FANO,
-		sserialize::ItemIndex::T_RLE_DE
+		sserialize::ItemIndex::T_RLE_DE,
+		sserialize::ItemIndex::T_NATIVE | __IT_MERGE_WITH_VECTOR,
+		sserialize::ItemIndex::T_WAH | __IT_MERGE_WITH_VECTOR,
+		sserialize::ItemIndex::T_RLE_DE | __IT_MERGE_WITH_VECTOR,
+		sserialize::ItemIndex::T_NATIVE | __IT_MERGE_WITH_HEAP,
+		sserialize::ItemIndex::T_WAH | __IT_MERGE_WITH_HEAP,
+		sserialize::ItemIndex::T_RLE_DE | __IT_MERGE_WITH_HEAP
 	});
 	
 	if (argc < 3 || argc % 2 != 0) {
