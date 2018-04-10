c8736876478efe5da94189bd359a4484307fb575
Daniel Bahrdt <daniel.git@funroll-loops.de>
2018-04-10 13:35:53 +0200

    [tests] OOMFlatTrie empty test

--- tests/containers_OOMFlatTrie.cpp
+++ tests/containers_OOMFlatTrie.cpp
@@ -1,2 +1,23 @@
-#include <sserialize/containers/OOMFlatTrie.h>
+#include "TestBase.h"
+#include <sserialize/search/OOMFlatTrie.h>
 
+class TestTemplate: public sserialize::tests::TestBase {
+CPPUNIT_TEST_SUITE( TestTemplate );
+CPPUNIT_TEST( test );
+CPPUNIT_TEST_SUITE_END();
+public:
+	virtual void setUp() {}
+	virtual void tearDown() {}
+	void test() {}
+};
+
+int main(int argc, char ** argv) {
+	srand( 0 );
+	
+	sserialize::tests::TestBase::init(argc, argv);
+	
+	CppUnit::TextUi::TestRunner runner;
+	runner.addTest(  TestTemplate::suite() );
+	bool ok = runner.run();
+	return ok ? 0 : 1;
+}
