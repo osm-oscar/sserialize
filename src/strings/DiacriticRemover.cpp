449f669b46c80ccb0432dbb8db33e9b337df783c
Daniel Bahrdt <daniel.git@funroll-loops.de>
2018-12-07 11:57:17 +0100

    [strings] DiacriticRemove: needs icu namespace access

--- src/strings/DiacriticRemover.cpp
+++ src/strings/DiacriticRemover.cpp
@@ -17,18 +17,18 @@ UErrorCode DiacriticRemover::init() {
 }
 
 void DiacriticRemover::transliterate(std::string& str) const {
-	UnicodeString ustr( UnicodeString::fromUTF8(str));
+	icu::UnicodeString ustr( icu::UnicodeString::fromUTF8(str));
 	m_transLiterator->transliterate(ustr);
 	str.clear();
-	StringByteSink<std::string> retStr(&str);
+	icu::StringByteSink<std::string> retStr(&str);
 	ustr.toUTF8(retStr);
 }
 
 std::string DiacriticRemover::operator()(const std::string& str) const {
-	UnicodeString ustr( UnicodeString::fromUTF8(str) );
+	icu::UnicodeString ustr( icu::UnicodeString::fromUTF8(str) );
 	m_transLiterator->transliterate(ustr);
 	std::string ret;
-	StringByteSink<std::string> retStr(&ret);
+	icu::StringByteSink<std::string> retStr(&ret);
 	ustr.toUTF8(retStr);
 	return ret;
 }
