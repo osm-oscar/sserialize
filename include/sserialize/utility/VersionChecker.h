5c6c7570a6549747675a67b5055ed9e9acb4ee57
Daniel Bahrdt <daniel.git@funroll-loops.de>
2019-07-09 17:12:27 +0200

    [util] VersionChecker improvements

--- include/sserialize/utility/VersionChecker.h
+++ include/sserialize/utility/VersionChecker.h
@@ -23,6 +23,16 @@ struct VersionChecker {
 		}
 		return d;
 	}
+	
+	//check version which is at the d[0]
+	inline static sserialize::UByteArrayAdapter
+	check(sserialize::UByteArrayAdapter const & d, uint8_t want, const char * msg) {
+		uint8_t have = d.getUint8(0);
+		if (UNLIKELY_BRANCH(want != have)) {
+			throw sserialize::VersionMissMatchException(std::string(msg), want, have);
+		}
+		return d+1;
+	}
 };
 
 
