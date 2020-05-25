95951dfa52ceb81d8fadbb4ec0af294d0071c648
Daniel Bahrdt <daniel.git@funroll-loops.de>
2020-05-25 10:56:38 +0200

    [tests] packfuncs: pop-protector support

--- tests/util_packfuncs.cpp
+++ tests/util_packfuncs.cpp
@@ -207,6 +207,11 @@ int main(int argc, char ** argv) {
 	srand( 0 );
 	CppUnit::TextUi::TestRunner runner;
 	runner.addTest(  PackFunctions::suite() );
+
+	if (sserialize::tests::TestBase::popProtector()) {
+		runner.eventManager().popProtector();
+	}
+
 	bool ok = runner.run();
 	return ok ? 0 : 1;
 }
